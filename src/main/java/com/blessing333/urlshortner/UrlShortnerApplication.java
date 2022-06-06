40dbea8bf88e92d4940f12af12c7475bd37fbed0
Minjae Lee <65841596+blessing333@users.noreply.github.com>
2022-06-06 20:37:19 +0900

    first commit

---
+++ src/main/java/com/blessing333/urlshortner/UrlShortnerApplication.java
@@ -0,0 +1,13 @@
+package com.blessing333.urlshortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortnerApplication.class, args);
+    }
+
+}
