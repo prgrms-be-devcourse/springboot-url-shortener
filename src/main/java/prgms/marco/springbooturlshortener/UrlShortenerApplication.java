e44608c9ed6f34439323717606ac63722fb301b4
hyuk0309 <bahngeh@naver.com>
2022-06-07 15:04:29 +0900

    chore(application.yml) : 설정 세팅

--- src/main/java/prgms/marco/springbooturlshortener/UrlShortenerApplication.java
+++ src/main/java/prgms/marco/springbooturlshortener/UrlShortenerApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootUrlShortenerApplication {
+public class UrlShortenerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootUrlShortenerApplication.class, args);
+        SpringApplication.run(UrlShortenerApplication.class, args);
     }
-
 }
