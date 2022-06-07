380522acf01b194c7d5d8789ac573d705e83e555
hyuk0309 <bahngeh@naver.com>
2022-06-07 14:27:43 +0900

    chore : init project

---
+++ src/test/java/prgms/marco/springbooturlshortener/SpringbootUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package prgms.marco.springbooturlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
