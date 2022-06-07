4ba018ced038ef5c0ab98fa460c989fe1828ba4f
sdardew <sdardew@gmail.com>
2022-06-07 17:01:39 +0900

    boolean existsUrlByShortUrl(String shortUrl) 구현
    단축 url이 테이블에 존재하는지 판단해주는 메서드
    단축 url이 테이블에 존재한다면 true
    아니라면 false를 리턴

--- src/main/java/com/sdardew/urlshortener/repository/UrlRepository.java
+++ src/main/java/com/sdardew/urlshortener/repository/UrlRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UrlRepository extends JpaRepository<Url, String> {
+
+  boolean existsUrlByShortUrl(String shortUrl);
 }
