d3017d7e31118615b831e27d33f45fce7a0f6a20
taesan <pawer02023@gmail.com>
2022-06-08 15:48:20 +0900

    feat: repository 구현
    repository 구현

---
+++ src/main/java/com/taehan/urlshortener/repository/UrlRepository.java
@@ -0,0 +1,7 @@
+package com.taehan.urlshortener.repository;
+
+import com.taehan.urlshortener.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UrlRepository extends JpaRepository<Url, Long> {
+}
