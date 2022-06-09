4669419d911a349d90a153178d854dc6151d3561
taesan <pawer02023@gmail.com>
2022-06-09 16:02:44 +0900

    chore: 주석 및 인덴트 수정
    - 주석 및 인덴트 수정

--- src/test/java/com/taehan/urlshortener/repository/UrlRepositoryTest.java
+++ src/test/java/com/taehan/urlshortener/repository/UrlRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
