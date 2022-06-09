094cffaef389e75d9f082b1161e65ea720f0a8fd
ordilov <ordilov@gmail.com>
2022-06-09 16:09:39 +0900

    init: 프로젝트를 초기 설정하라

---
+++ src/main/java/org/prgrms/kdt/url/UrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.kdt.url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UrlShortenerApplication.class, args);
+  }
+
+}
