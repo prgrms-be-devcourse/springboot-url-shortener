14a5b1c0b6eb27c872e5e68e701b74d23ea0d170
kkj0419 <kkhi0419@gmail.com>
2022-06-09 23:33:12 +0900

    test : Base62Utils

--- src/main/java/com/kdt/shortener/utils/Base62Utils.java
+++ src/main/java/com/kdt/shortener/utils/Base62Utils.java
@@ -1,21 +1,17 @@
 package com.kdt.shortener.utils;
 
-import org.apache.logging.log4j.util.Chars;
-import org.yaml.snakeyaml.util.ArrayUtils;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Base62 {
+public class Base62Utils {
 
     final int RADIX = 62;
-    final char[] CODE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+
+    final String CODE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     public String encoding(long id) {
         StringBuffer sb = new StringBuffer();
         while (id > 0) {
-            sb.append(CODE[(int) id % RADIX]);
+            sb.append(CODE.charAt((int) id % RADIX));
             id /= RADIX;
         }
         return sb.toString();
@@ -25,7 +21,7 @@ public class Base62 {
         long result = 0;
         long power = 1;
         for (int i = 0; i < shortUrl.length(); i++) {
-            result += Arrays.asList(CODE).indexOf(shortUrl.charAt(i)) * power;
+            result += CODE.indexOf(shortUrl.charAt(i)) * power;
             power *= RADIX;
         }
         return result;
