8ac50068553f48fa6fc44ca1bad66ef9546c2748
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-06-10 21:38:11 +0900

    :sparkles: feat: 기본 URL 컨트롤러 추가

---
+++ src/main/java/com/urlshortener/shorturl/controller/ShortUrlController.java
@@ -0,0 +1,34 @@
+package com.urlshortener.shorturl.controller;
+
+import com.urlshortener.shorturl.model.dto.CreateRequest;
+import com.urlshortener.shorturl.model.dto.CreateResponse;
+import com.urlshortener.shorturl.service.UrlService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
+@RequiredArgsConstructor
+public class ShortUrlController {
+    private final UrlService urlService;
+
+    @PostMapping
+    public ResponseEntity<CreateResponse> registerUrl(@RequestBody CreateRequest request) {
+        CreateResponse response = urlService.save(request);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/{url}")
+    public ResponseEntity<String> getTest(@PathVariable("url") String url) throws URISyntaxException {
+        String originUrl = urlService.findOne(url);
+        URI redirectUri = new URI(originUrl);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(redirectUri);
+        return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
+    }
+}
