64364075cfc7c1404daee733fd8ff2fe688ab75a
sdardew <sdardew@gmail.com>
2022-06-10 17:54:04 +0900

    feat: URL 존재하는지 아닌지 확인 가능

--- src/main/java/com/sdardew/urlshortener/service/UrlUtils.java
+++ src/main/java/com/sdardew/urlshortener/service/UrlUtils.java
@@ -1,73 +1,39 @@
 package com.sdardew.urlshortener.service;
 
-import org.aspectj.weaver.ast.Call;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
 
 import java.io.IOException;
 import java.net.*;
-import java.util.Timer;
-import java.util.concurrent.*;
 
 @Component
 public class UrlUtils {
 
-  public boolean isExistingUrl(String originalUrl) throws IOException {
-    if(checkFormat(originalUrl) && checkConnection(originalUrl)) {
-      return true;
+  public void checkUrl(String originalUrl) throws IOException {
+    if(!checkFormat(originalUrl)) {
+      throw new MalformedURLException();
     }
-    throw new MalformedURLException();
+    if (!checkExistingUrl(originalUrl)) {
+      throw new UnknownHostException();
+    }
   }
 
   private boolean checkFormat(String originalUrl) {
     return originalUrl.matches("^(https?:\\/\\/)?([\\da-z\\.-]+\\.[a-z\\.]{2,6}|[\\d\\.]+)([\\/:?=&#]{1}[\\da-z\\.-]+)*[\\/\\?]?$");
   }
 
-  private boolean checkConnection(String originalUrl) throws IOException {
-
-    int responseCode;
-
-    try {
-      responseCode = getResponseCode(originalUrl);
-    } catch (SocketTimeoutException e) {
-      throw new UnknownHostException(originalUrl + " is unknown host");
-    }
-
-    if(HttpURLConnection.HTTP_OK != responseCode) {
-      throw new UnknownHostException(originalUrl + " is unknown host");
-    }
-
-    return true;
-  }
-
-  public boolean exist(String loc) {
-    try {
-      HttpURLConnection connection = (HttpURLConnection) new URL(loc).openConnection();
-      connection.setRequestMethod("HEAD");
-      return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
-    } catch (ProtocolException e) {
-      return false;
-    } catch (MalformedURLException e) {
-      return false;
-    } catch (IOException e) {
-      return false;
-    }
-  }
-
-  private int getResponseCode(String originalUrl) throws IOException {
+  private boolean checkExistingUrl(String originalUrl) throws IOException {
     URL url = new URL(originalUrl);
     HttpURLConnection huc = (HttpURLConnection) url.openConnection();
     huc.setRequestMethod("HEAD");
     huc.setConnectTimeout(1_000);
-    int responseCode = HttpURLConnection.HTTP_CLIENT_TIMEOUT;
+    int responseCode;
     try {
       responseCode = huc.getResponseCode();
     } catch (SocketTimeoutException e) {
-//      e.printStackTrace();
+      responseCode = HttpURLConnection.HTTP_CLIENT_TIMEOUT;
     } finally {
       huc.disconnect();
     }
-    return responseCode;
+    return responseCode == HttpURLConnection.HTTP_OK;
   }
 }
--- src/test/java/com/sdardew/urlshortener/service/UrlUtilsTest.java
+++ src/test/java/com/sdardew/urlshortener/service/UrlUtilsTest.java
@@ -3,19 +3,10 @@ package com.sdardew.urlshortener.service;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UrlUtilsTest {
@@ -31,20 +22,14 @@ class UrlUtilsTest {
   @DisplayName("url이 존재하는지 확인 가능하다")
   void testIsExistingUrl() {
     assertAll(
-      () -> assertThat(urlUtils.isExistingUrl("https://www.google.com/")).isTrue(),
+      () -> assertDoesNotThrow(
+        () -> urlUtils.checkUrl("https://www.google.com/")),
       () -> assertThrows(
         MalformedURLException.class,
-        () -> urlUtils.isExistingUrl("www.google.com")),
+        () -> urlUtils.checkUrl("www.google.com")),
       () -> assertThrows(
         UnknownHostException.class,
-        () -> urlUtils.isExistingUrl("http://localhost.com"))
+        () -> urlUtils.checkUrl("http://localhost.com"))
       );
-  }
-
-  @Test
-  @DisplayName("display")
-  void test() {
-
-
   }
 }
