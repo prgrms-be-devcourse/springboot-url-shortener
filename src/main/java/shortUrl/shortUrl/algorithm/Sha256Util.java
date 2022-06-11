5449d80864f1908c4de5a735be467dde47f3b4de
Hyunggeun447 <bopi447@naver.com>
2022-06-12 03:20:22 +0900

    sha256 - I,l,0,O,1,o 제거, OriginalUrl valdate 추가

--- src/main/java/shortUrl/shortUrl/algorithm/Sha256Util.java
+++ src/main/java/shortUrl/shortUrl/algorithm/Sha256Util.java
@@ -2,8 +2,12 @@ package shortUrl.shortUrl.algorithm;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sha256Util {
+
+    private final List<String> PASS_STRING = new ArrayList<>(List.of("0", "O", "I", "l", "1", "o"));
 
     public String encoding(Long value){
         String msg = value.toString();
@@ -21,7 +25,11 @@ public class Sha256Util {
     private String bytesToHex(byte[] bytes) {
         StringBuilder stringBuilder = new StringBuilder();
         for (byte b : bytes) {
-            stringBuilder.append(String.format("%02x", b));
+            String string = String.format("%02x", b);
+            if (PASS_STRING.contains(string)) {
+                continue;
+            }
+            stringBuilder.append(string);
         }
         return stringBuilder.toString();
     }
--- src/main/java/shortUrl/shortUrl/domain/service/UrlService.java
+++ src/main/java/shortUrl/shortUrl/domain/service/UrlService.java
@@ -1,8 +1,12 @@
 package shortUrl.shortUrl.domain.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 import shortUrl.shortUrl.domain.entity.Url;
 import shortUrl.shortUrl.domain.repository.UrlRepository;
 import shortUrl.shortUrl.domain.value.Algorithm;
@@ -10,6 +14,7 @@ import shortUrl.shortUrl.domain.dto.CreateShortUrlDto;
 import shortUrl.shortUrl.domain.dto.ShortUrlDto;
 import shortUrl.shortUrl.exception.AlreadyExistException;
 import shortUrl.shortUrl.exception.NotExistException;
+import shortUrl.shortUrl.exception.WrongUrlException;
 
 import java.util.Optional;
 
@@ -28,6 +33,10 @@ public class UrlService {
     public String createShortUrl(CreateShortUrlDto createShortUrlDto) {
         String originalUrl = createShortUrlDto.getOriginalUrl();
         Algorithm algorithm = createShortUrlDto.getAlgorithm();
+
+        if (!validateUrl(originalUrl)) {
+            throw new WrongUrlException("잘못된 url 입력입니다.");
+        }
 
         // TODO: 2022/06/11 이미 존재하는 url이 있을경우 - 코드 개선 방향이 있는가?
         Optional<Url> existUrl = urlRepository.findByOriginalUrlAndAlgorithm(originalUrl, algorithm);
@@ -59,7 +68,8 @@ public class UrlService {
     }
 
     @Transactional(readOnly = true)
-    public ShortUrlDto getUrlInfo(String shortUrl) {
+    public ShortUrlDto getUrlInfo(ShortUrlDto shortUrlDto) {
+        String shortUrl = shortUrlDto.getShortUrl();
         Url url = urlRepository.findByShortUrl(shortUrl)
                         .orElseThrow(() -> new NotExistException("존재하지 않습니다"));
         return ShortUrlDto.builder()
@@ -67,5 +77,13 @@ public class UrlService {
                 .hits(url.getHits())
                 .algorithm(url.getAlgorithm())
                 .build();
+    }
+
+    private boolean validateUrl(String originalUrl) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.exchange(originalUrl, HttpMethod.HEAD, null, String.class);
+
+        HttpStatus statusCode = responseEntity.getStatusCode();
+        return statusCode.is2xxSuccessful();
     }
 }
