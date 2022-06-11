20e5bc01fb0e7566acc6e32de87af9024f50a99c
kkj0419 <kkhi0419@gmail.com>
2022-06-12 00:03:30 +0900

    feat : form validation

--- src/main/java/com/kdt/shortener/controller/UrlController.java
+++ src/main/java/com/kdt/shortener/controller/UrlController.java
@@ -5,8 +5,11 @@ import com.kdt.shortener.service.UrlInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,20 +18,24 @@ public class UrlController {
     private final UrlInfoService service;
 
     @PostMapping("/convert")
-    public String convertUrl(Model model, UrlForm urlForm) {
+    public String convertUrl(Model model, @Valid UrlForm urlForm, BindingResult bindingResult) {
 
-        String result;
+        if (bindingResult.hasErrors()) {
+            throw new IllegalArgumentException();
+        }
+
+        String urlResult;
         if("S".equals(urlForm.getConvertType())){
-            result = service.makeShortUrl(urlForm.getUrlValue());
+            urlResult = service.makeShortUrl(urlForm.getUrlValue());
         }else if ("L".equals(urlForm.getConvertType())) {
             var info = service.turnOriginUrl(urlForm.getUrlValue());
-            result = info.getOriginUrl();
+            urlResult = info.getOriginUrl();
         } else {
             throw new IllegalArgumentException();
         }
 
         model.addAttribute("urlForm", new UrlForm());
-        model.addAttribute("result", result);
+        model.addAttribute("urlResult", urlResult);
         return "index";
     }
 
--- src/main/java/com/kdt/shortener/domain/UrlForm.java
+++ src/main/java/com/kdt/shortener/domain/UrlForm.java
@@ -3,9 +3,14 @@ package com.kdt.shortener.domain;
 import lombok.Data;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Data
 public class UrlForm {
+    @NotBlank(message = "URL 값을 비워둘 수 없습니다.")
     private String urlValue;
+
+    @NotBlank(message = "잘못된 타입 값입니다.")
     private String convertType;
 }
