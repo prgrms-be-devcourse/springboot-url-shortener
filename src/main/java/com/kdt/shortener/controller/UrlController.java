d0b72cad31e68610354f97c88940668849337cf2
kkj0419 <kkhi0419@gmail.com>
2022-06-12 00:19:33 +0900

    feat : shortUrl redirect 기능 추가

--- src/main/java/com/kdt/shortener/controller/UrlController.java
+++ src/main/java/com/kdt/shortener/controller/UrlController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -17,23 +18,14 @@ public class UrlController {
 
     private final UrlInfoService service;
 
-    @PostMapping("/convert")
+    @PostMapping("/url/convert")
     public String convertUrl(Model model, @Valid UrlForm urlForm, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
             throw new IllegalArgumentException();
         }
 
-        String urlResult;
-        if("S".equals(urlForm.getConvertType())){
-            urlResult = service.makeShortUrl(urlForm.getUrlValue());
-        }else if ("L".equals(urlForm.getConvertType())) {
-            var info = service.turnOriginUrl(urlForm.getUrlValue());
-            urlResult = info.getOriginUrl();
-        } else {
-            throw new IllegalArgumentException();
-        }
-
+        String urlResult = service.makeShortUrl(urlForm.getUrlValue());
         model.addAttribute("urlForm", new UrlForm());
         model.addAttribute("urlResult", urlResult);
         return "index";
@@ -42,7 +34,12 @@ public class UrlController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("urlForm", new UrlForm());
-
         return "index";
+    }
+
+    @GetMapping("/{shortUrl}")
+    public String redirectPage(@PathVariable String shortUrl) {
+        String longUrl = service.turnOriginUrl(shortUrl).getOriginUrl();
+        return "redirect:" + longUrl;
     }
 }
--- src/main/java/com/kdt/shortener/domain/UrlForm.java
+++ src/main/java/com/kdt/shortener/domain/UrlForm.java
@@ -11,6 +11,4 @@ public class UrlForm {
     @NotBlank(message = "URL 값을 비워둘 수 없습니다.")
     private String urlValue;
 
-    @NotBlank(message = "잘못된 타입 값입니다.")
-    private String convertType;
 }
