5c0839d536006b5657ab0037268837bf7f76ee41
kkj0419 <kkhi0419@gmail.com>
2022-06-11 23:51:44 +0900

    refactor : 시그니처 변경

--- src/main/java/com/kdt/shortener/service/UrlInfoService.java
+++ src/main/java/com/kdt/shortener/service/UrlInfoService.java
@@ -15,18 +15,20 @@ public class UrlInfoService {
     private final Base62Utils base62Utils;
 
     public UrlInfo turnOriginUrl(String shortUrl) {
-        Long id = base62Utils.decoding(shortUrl);
+        var arr = shortUrl.split("/");
+        String hash = arr[arr.length - 1];
+        Long id = base62Utils.decoding(hash);
         return repository.findById(id).orElseThrow(IllegalArgumentException::new);
     }
 
     @Transactional
-    public String makeShortUrl(String originUrl){
+    public String makeShortUrl(String originUrl) {
         var findOne = repository.findByOriginUrl(originUrl);
         if (findOne.isPresent()) {
             return base62Utils.encoding(findOne.get().getId());
         }
         UrlInfo urlInfo = new UrlInfo(originUrl);
         repository.save(urlInfo);
-        return base62Utils.encoding(urlInfo.getId());
+        return "http://localhost:8080/" + base62Utils.encoding(urlInfo.getId());
     }
 }
