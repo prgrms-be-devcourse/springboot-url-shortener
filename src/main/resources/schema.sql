8d8dfb33199e9854c796e68525ba778ceba68260
kkj0419 <kkhi0419@gmail.com>
2022-06-11 23:49:04 +0900

    chore : database setting

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE SEQUENCE hibernate_sequence start with 100001 increment by 1;
+
+CREATE TABLE urlinfo
+(
+    id bigint auto_increment primary key,
+    origin_url varchar(200) not null unique
+);
+
