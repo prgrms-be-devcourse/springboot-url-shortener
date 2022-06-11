a5d51d04761c275a173a660c55759e273dc214b8
kkj0419 <kkhi0419@gmail.com>
2022-06-11 22:18:43 +0900

    feat : ID 필드 generation type 지정

--- src/main/java/com/kdt/shortener/domain/UrlInfo.java
+++ src/main/java/com/kdt/shortener/domain/UrlInfo.java
@@ -3,9 +3,7 @@ package com.kdt.shortener.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
@@ -14,6 +12,7 @@ import javax.persistence.Table;
 public class UrlInfo {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String originUrl;
--- src/main/java/com/kdt/shortener/repository/UrlInfoRepository.java
+++ src/main/java/com/kdt/shortener/repository/UrlInfoRepository.java
@@ -2,11 +2,9 @@ package com.kdt.shortener.repository;
 
 import com.kdt.shortener.domain.UrlInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UrlInfoRepository extends JpaRepository<UrlInfo, Long> {
 
     Optional<UrlInfo> findByOriginUrl(String originUrl);
