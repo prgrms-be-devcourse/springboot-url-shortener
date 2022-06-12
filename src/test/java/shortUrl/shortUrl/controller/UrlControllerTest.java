82c9da7a5864724b10888e5ed707d425ad8d520c
Hyunggeun447 <bopi447@naver.com>
2022-06-12 19:03:17 +0900

    fix : controllerTest 실패사례 + 분류

--- src/test/java/shortUrl/shortUrl/controller/UrlControllerTest.java
+++ src/test/java/shortUrl/shortUrl/controller/UrlControllerTest.java
@@ -1,9 +1,6 @@
 package shortUrl.shortUrl.controller;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.*;
@@ -66,7 +63,7 @@ class UrlControllerTest {
         }
 
         @Test
-        @DisplayName("실패 : POST /create -> 입력 url이 유효하지 않을 때")
+        @DisplayName("실패 : POST /create => 입력 url이 유효하지 않을 때")
         public void Test1() throws Exception {
 
             //given
@@ -86,87 +83,180 @@ class UrlControllerTest {
         }
     }
 
-    @Test
-    @DisplayName("성공 : POST /link")
-    public void link() throws Exception {
+    @Nested
+    class 단축_url_바로가기 {
 
-        //given
-        RestTemplate restTemplate = new RestTemplate();
+        @Test
+        @DisplayName("성공 : POST /link")
+        public void link() throws Exception {
 
-        /**
-         * short url 생성
-         */
-        String longUrl = "https://www.naver.com/";
-        CreateShortUrlDto createShortUrlDto = new CreateShortUrlDto(longUrl, Algorithm.BASE_56);
-        String shortUrl = urlService.createShortUrl(createShortUrlDto).getShortUrl();
+            //given
+            RestTemplate restTemplate = new RestTemplate();
 
-        //when
-        String url = "http://localhost:8080/link";
-        ShortUrlDto shortUrlDto = new ShortUrlDto();
-        shortUrlDto.setShortUrl(shortUrl);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, shortUrlDto, String.class);
+            /**
+             * short url 생성
+             */
+            String longUrl = "https://www.naver.com/";
+            CreateShortUrlDto createShortUrlDto = new CreateShortUrlDto(longUrl, Algorithm.BASE_56);
+            String shortUrl = urlService.createShortUrl(createShortUrlDto).getShortUrl();
 
-        //then
-        HttpStatus statusCode = response.getStatusCode();
-        assertThat(statusCode).isEqualTo(HttpStatus.FOUND);
+            //when
+            String url = "http://localhost:8080/link";
+            ShortUrlDto shortUrlDto = new ShortUrlDto();
+            shortUrlDto.setShortUrl(shortUrl);
+            ResponseEntity<String> response = restTemplate.postForEntity(url, shortUrlDto, String.class);
+
+            //then
+            HttpStatus statusCode = response.getStatusCode();
+            assertThat(statusCode).isEqualTo(HttpStatus.FOUND);
+        }
+
+        @Test
+        @DisplayName("실패 : POST /link -> 잘못된 short url")
+        public void link_fail() throws Exception {
+
+            //given
+            RestTemplate restTemplate = new RestTemplate();
+
+            /**
+             * short url 생성
+             */
+            String longUrl = "https://www.naver.com/";
+            CreateShortUrlDto createShortUrlDto = new CreateShortUrlDto(longUrl, Algorithm.BASE_56);
+            String shortUrl = urlService.createShortUrl(createShortUrlDto).getShortUrl();
+
+            //when
+            String wrongUrl = "wrongUrl";
+
+            String url = "http://localhost:8080/link";
+            ShortUrlDto shortUrlDto = new ShortUrlDto();
+            shortUrlDto.setShortUrl(wrongUrl);
+
+            //then
+            assertThrows(RuntimeException.class, () -> restTemplate.postForEntity(url, shortUrlDto, String.class));
+        }
     }
 
-    @Test
-    @DisplayName("성공 POST /{shortUrl}")
-    public void goShortUrl() throws Exception {
+    @Nested
+    class 주소창에_shortUrl_입력{
+        @Test
+        @DisplayName("성공 : POST /{shortUrl}")
+        public void goShortUrl() throws Exception {
 
-        //given
-        RestTemplate restTemplate = new RestTemplate();
+            //given
+            RestTemplate restTemplate = new RestTemplate();
 
-        /**
-         * short url 생성
-         */
-        String longUrl = "https://www.naver.com/";
-        CreateShortUrlDto createShortUrlDto = new CreateShortUrlDto(longUrl, Algorithm.BASE_56);
-        String shortUrl = urlService.createShortUrl(createShortUrlDto).getShortUrl();
+            /**
+             * short url 생성
+             */
+            String longUrl = "https://www.naver.com/";
+            CreateShortUrlDto createShortUrlDto = new CreateShortUrlDto(longUrl, Algorithm.BASE_56);
+            String shortUrl = urlService.createShortUrl(createShortUrlDto).getShortUrl();
 
-        //when
-        URI uri = UriComponentsBuilder
-                .fromUriString("http://localhost:8080")
-                .path("/{shortUrl}")
-                .encode()
-                .build()
-                .expand(shortUrl)
-                .toUri();
-        ResponseEntity<String> response = restTemplate.postForEntity(uri, shortUrl, String.class);
+            //when
+            URI uri = UriComponentsBuilder
+                    .fromUriString("http://localhost:8080")
+                    .path("/{shortUrl}")
+                    .encode()
+                    .build()
+                    .expand(shortUrl)
+                    .toUri();
+            ResponseEntity<String> response = restTemplate.postForEntity(uri, shortUrl, String.class);
 
-        //then
-        HttpStatus statusCode = response.getStatusCode();
-        assertThat(statusCode).isEqualTo(HttpStatus.FOUND);
+            //then
+            HttpStatus statusCode = response.getStatusCode();
+            assertThat(statusCode).isEqualTo(HttpStatus.FOUND);
+        }
+
+        @Test
+        @DisplayName("실패 : POST /{shortUrl} => 잘못된 short url")
+        public void goShortUrl_fail() throws Exception {
+
+            //given
+            RestTemplate restTemplate = new RestTemplate();
+
+            /**
+             * short url 생성
+             */
+            String longUrl = "https://www.naver.com/";
+            CreateShortUrlDto createShortUrlDto = new CreateShortUrlDto(longUrl, Algorithm.BASE_56);
+            String shortUrl = urlService.createShortUrl(createShortUrlDto).getShortUrl();
+
+            //when
+            String wrongUrl = "wrongUrl";
+
+            URI uri = UriComponentsBuilder
+                    .fromUriString("http://localhost:8080")
+                    .path("/{shortUrl}")
+                    .encode()
+                    .build()
+                    .expand(wrongUrl)
+                    .toUri();
+
+            //then
+            assertThrows(RuntimeException.class, () -> restTemplate.postForEntity(uri, shortUrl, String.class));
+        }
     }
 
-    @Test
-    @DisplayName("성공 : GET /{shortUrl}/info")
-    public void Test() throws Exception {
+    @Nested
+    class shortUrl_정보조회{
+        @Test
+        @DisplayName("성공 : GET /{shortUrl}/info")
+        public void Test() throws Exception {
 
-        //given
-        RestTemplate restTemplate = new RestTemplate();
+            //given
+            RestTemplate restTemplate = new RestTemplate();
 
-        /**
-         * short url 생성
-         */
-        String longUrl = "https://www.naver.com/";
-        CreateShortUrlDto createShortUrlDto = new CreateShortUrlDto(longUrl, Algorithm.BASE_56);
-        String shortUrl = urlService.createShortUrl(createShortUrlDto).getShortUrl();
+            /**
+             * short url 생성
+             */
+            String longUrl = "https://www.naver.com/";
+            CreateShortUrlDto createShortUrlDto = new CreateShortUrlDto(longUrl, Algorithm.BASE_56);
+            String shortUrl = urlService.createShortUrl(createShortUrlDto).getShortUrl();
 
-        //when
-        URI uri = UriComponentsBuilder
-                .fromUriString("http://localhost:8080")
-                .path("/{shortUrl}/info")
-                .encode()
-                .build()
-                .expand(shortUrl)
-                .toUri();
-        ShortUrlDto shortUrlDto = restTemplate.getForObject(uri, ShortUrlDto.class);
+            //when
+            URI uri = UriComponentsBuilder
+                    .fromUriString("http://localhost:8080")
+                    .path("/{shortUrl}/info")
+                    .encode()
+                    .build()
+                    .expand(shortUrl)
+                    .toUri();
+            ShortUrlDto shortUrlDto = restTemplate.getForObject(uri, ShortUrlDto.class);
 
-        //then
-        assertThat(shortUrlDto.getOriginalUrl()).isEqualTo(longUrl);
-        assertThat(shortUrlDto.getAlgorithm()).isEqualTo(Algorithm.BASE_56);
-        assertThat(shortUrlDto.getHits()).isEqualTo(0L);
+            //then
+            assertThat(shortUrlDto.getOriginalUrl()).isEqualTo(longUrl);
+            assertThat(shortUrlDto.getAlgorithm()).isEqualTo(Algorithm.BASE_56);
+            assertThat(shortUrlDto.getHits()).isEqualTo(0L);
+        }
+
+        @Test
+        @DisplayName("실패 : GET /{shortUrl}/info => 잘못된 url")
+        public void getInfo_fail() throws Exception {
+
+            //given
+            RestTemplate restTemplate = new RestTemplate();
+
+            /**
+             * short url 생성
+             */
+            String longUrl = "https://www.naver.com/";
+            CreateShortUrlDto createShortUrlDto = new CreateShortUrlDto(longUrl, Algorithm.BASE_56);
+            String shortUrl = urlService.createShortUrl(createShortUrlDto).getShortUrl();
+
+            //when
+            String wrongUrl = "wrongUrl";
+
+            URI uri = UriComponentsBuilder
+                    .fromUriString("http://localhost:8080")
+                    .path("/{shortUrl}/info")
+                    .encode()
+                    .build()
+                    .expand(wrongUrl)
+                    .toUri();
+
+            //then
+            assertThrows(RuntimeException.class, () -> restTemplate.getForObject(uri, ShortUrlDto.class));
+        }
     }
 }
