cfbeaf119ca04810ff97d3bd9b5d82a352b77aee
kkj0419 <kkhi0419@gmail.com>
2022-06-13 02:17:39 +0900

    test : service exception

--- src/test/java/com/kdt/shortener/service/UrlInfoServiceTest.java
+++ src/test/java/com/kdt/shortener/service/UrlInfoServiceTest.java
@@ -9,8 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -40,6 +41,18 @@ class UrlInfoServiceTest {
 
         //then
         assertThat(result.getOriginUrl(), is(originUrl));
+    }
+
+    @Test
+    @DisplayName("잘못된 shortURL 입력 테스트")
+    void illegalShortUrlInputTest(){
+
+        //given
+        String wrongUrl = "http://localhost:8080/short/ERROR";
+
+        assertThrows(IllegalArgumentException.class, () ->
+                service.turnOriginUrl(wrongUrl));
+
     }
 
 }
