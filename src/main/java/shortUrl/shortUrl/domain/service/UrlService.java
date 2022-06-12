45e11bf18219e44b059a4888249a39294c8601eb
Hyunggeun447 <bopi447@naver.com>
2022-06-13 01:00:19 +0900

    service 리팩토링?

--- src/main/java/shortUrl/shortUrl/domain/service/UrlService.java
+++ src/main/java/shortUrl/shortUrl/domain/service/UrlService.java
@@ -38,7 +38,7 @@ public class UrlService {
             throw new WrongUrlException("잘못된 url 입력입니다.");
         }
 
-        // TODO: 2022/06/11 이미 존재하는 url이 있을경우 - 코드 개선 방향이 있는가?
+        // TODO: 2022/06/11 코드 개선 방향이 있는가?
         Optional<Url> existUrl = urlRepository.findByOriginalUrlAndAlgorithm(originalUrl, algorithm);
         if (existUrl.isPresent()) {
             log.info("algorithm[{}] 을 사용한 동일한 originalUrl[{}] 이 존재합니다.", algorithm, originalUrl);
@@ -46,22 +46,18 @@ public class UrlService {
             return shortUrlDto;
         }
 
-        Url url = new Url(originalUrl, algorithm);
-        Url savedUrl = urlRepository.save(url);
-
-        Long id = savedUrl.getId();
-        String shortUrl = algorithm.encoding(id);
+        Url savedUrl = urlRepository.save(new Url(originalUrl, algorithm));
+        String shortUrl = algorithm.encoding(savedUrl.getId());
 
         // TODO: 2022/06/12 존재하면 그 후 처리는? error log를 찍고 예외 처리가 아닌 char length가 +1인 url을 반환 후 보수?
-        if (urlRepository.existsByShortUrl(shortUrl)) {
-            log.error("중복되는 shortUrl 발생!!! originalUrl => {}, algorithm => {}, shortUrl => {}",
-                    originalUrl, algorithm, shortUrl);
-            throw new AlreadyExistException("이미 존재하는 short url 입니다.");
-        }
+        checkSameShortUrl(originalUrl, algorithm, shortUrl);
+
         savedUrl.saveShortUrl(shortUrl);
         shortUrlDto.setShortUrl(shortUrl);
         return shortUrlDto;
     }
+
+
 
     public String findOriginUrlByShortUrl(String shortUrl) {
         Url url = urlRepository.findByShortUrl(shortUrl)
@@ -93,5 +89,13 @@ public class UrlService {
                 .getStatusCode();
         System.out.println("statusCode = " + statusCode);
         return statusCode.is2xxSuccessful() || statusCode.is3xxRedirection();
+    }
+
+    private void checkSameShortUrl(String originalUrl, Algorithm algorithm, String shortUrl) {
+        if (urlRepository.existsByShortUrl(shortUrl)) {
+            log.error("중복되는 shortUrl 발생!!! originalUrl => {}, algorithm => {}, shortUrl => {}",
+                    originalUrl, algorithm, shortUrl);
+            throw new AlreadyExistException("이미 존재하는 short url 입니다.");
+        }
     }
 }
