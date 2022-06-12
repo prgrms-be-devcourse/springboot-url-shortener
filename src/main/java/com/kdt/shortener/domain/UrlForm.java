e57e43680c5c4c37119dec2a2d3e29accba60711
kkj0419 <kkhi0419@gmail.com>
2022-06-12 23:11:19 +0900

    test : UrlForm validation

--- src/main/java/com/kdt/shortener/domain/UrlForm.java
+++ src/main/java/com/kdt/shortener/domain/UrlForm.java
@@ -4,10 +4,12 @@ import lombok.Data;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Data
 public class UrlForm {
     @NotBlank
+    @Pattern(regexp="^((http(s?))\\:\\/\\/)([0-9a-zA-Z\\-]+\\.)+[a-zA-Z]{2,6}(\\:[0-9]+)?(\\/\\S*)?$")
     private String urlValue;
 }
