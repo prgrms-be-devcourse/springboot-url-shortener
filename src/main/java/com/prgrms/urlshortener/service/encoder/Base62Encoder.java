db1fb1e3a9467e26109ffddd7ef314c694b6a1ab
yuminhwan <ymh7532@icloud.com>
2022-06-13 01:11:47 +0900

    refactor: BASE62_FORMAT String -> char 배열로 리팩터링

--- src/main/java/com/prgrms/urlshortener/service/encoder/Base62Encoder.java
+++ src/main/java/com/prgrms/urlshortener/service/encoder/Base62Encoder.java
@@ -6,13 +6,13 @@ import org.springframework.stereotype.Component;
 public class Base62Encoder implements UrlEncoder {
 
     private static final int BASE62 = 62;
-    private static final String BASE62_FORMAT = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final char[] BASE62_FORMAT = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 
     @Override
     public String encode(long id) {
         StringBuilder shortUrl = new StringBuilder();
         while (id > 0) {
-            shortUrl.append(BASE62_FORMAT.charAt((int)(id % BASE62)));
+            shortUrl.append(BASE62_FORMAT[((int)(id % BASE62))]);
             id /= BASE62;
         }
         return shortUrl.reverse().toString();
