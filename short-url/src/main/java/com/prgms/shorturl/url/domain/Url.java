1c05b7a575cea38cbda4fdf41533fa41bba1ff58
ChoiYeonho0903 <903yh@naver.com>
2022-06-13 19:17:22 +0900

    Url Entity 수정

---
+++ short-url/src/main/java/com/prgms/shorturl/url/domain/Url.java
@@ -0,0 +1,40 @@
+package com.prgms.shorturl.url.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "url")
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Url {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(name = "long_url", nullable = false)
+    private String longUrl;
+
+    @Column(name = "call_count", nullable = false)
+    private long callCount = 0;
+
+    public Url(String longUrl) {
+        this.longUrl = longUrl;
+    }
+
+    public Url(Long id, String longUrl) {
+        this.id = id;
+        this.longUrl = longUrl;
+    }
+
+    public long addCallCount() {
+        return callCount++;
+    }
+}
