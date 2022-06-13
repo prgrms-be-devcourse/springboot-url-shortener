4097c7f991d7de791f2c4b07b75f5de6c3aa8ec9
ChoiYeonho0903 <903yh@naver.com>
2022-06-13 19:20:52 +0900

    UrlRequestDto, UrlResponseDto 구현

---
+++ short-url/src/main/java/com/prgms/shorturl/url/dto/UrlRequestDto.java
@@ -0,0 +1,16 @@
+package com.prgms.shorturl.url.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UrlRequestDto {
+
+    private String longUrl;
+}
