b3f089196b034f509dc6f88f86d4c248972c2394
yuminhwan <ymh7532@icloud.com>
2022-06-14 22:44:45 +0900

    style: 변수명 변경

--- src/main/java/com/prgrms/urlshortener/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/prgrms/urlshortener/common/exception/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     private static final String ERROR_LOG = "[ERROR] %s : %s";
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -19,7 +19,7 @@ public class GlobalExceptionHandler {
         MethodArgumentNotValidException e
     ) {
         String errorMessage = extractErrorMessage(e);
-        log.info(String.format(ERROR_LOG, e.getClass().getSimpleName(), errorMessage), e);
+        logger.info(String.format(ERROR_LOG, e.getClass().getSimpleName(), errorMessage), e);
         return ResponseEntity
             .badRequest()
             .body(new ErrorResponse(errorMessage));
@@ -34,7 +34,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<ErrorResponse> handleApplicationException(ApplicationException e) {
-        log.info(String.format(ERROR_LOG, e.getClass().getSimpleName(), e.getMessage()), e);
+        logger.info(String.format(ERROR_LOG, e.getClass().getSimpleName(), e.getMessage()), e);
         return ResponseEntity
             .status(e.getHttpStatus())
             .body(new ErrorResponse(e.getMessage()));
@@ -42,7 +42,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.error(String.format(ERROR_LOG, e.getClass().getSimpleName(), e.getMessage()), e);
+        logger.error(String.format(ERROR_LOG, e.getClass().getSimpleName(), e.getMessage()), e);
         return ResponseEntity
             .status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(new ErrorResponse("서버에서 에러가 발생했습니다."));
