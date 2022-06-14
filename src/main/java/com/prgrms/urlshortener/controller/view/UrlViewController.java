4d4cce43da47ceaab3de39094337a7c49de48ee1
yuminhwan <ymh7532@icloud.com>
2022-06-14 23:18:00 +0900

    refactor: 불필요한 의존성 제거

--- src/main/java/com/prgrms/urlshortener/controller/view/UrlViewController.java
+++ src/main/java/com/prgrms/urlshortener/controller/view/UrlViewController.java
@@ -3,16 +3,8 @@ package com.prgrms.urlshortener.controller.view;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.prgrms.urlshortener.service.UrlService;
-
 @Controller
 public class UrlViewController {
-
-    private final UrlService urlService;
-
-    public UrlViewController(UrlService urlService) {
-        this.urlService = urlService;
-    }
 
     @GetMapping("/")
     public String viewUrlPage() {
