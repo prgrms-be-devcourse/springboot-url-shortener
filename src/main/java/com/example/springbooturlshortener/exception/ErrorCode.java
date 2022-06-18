c674a898f1018ef21792baafc35c48f42b1587f1
hayoung <>
2022-06-18 11:21:08 +0900

    Revert "Revert "feat: 예외 처리 구현""
    This reverts commit 63292fc8d0437a50d39377fe718ed37b05590f98.

---
+++ src/main/java/com/example/springbooturlshortener/exception/ErrorCode.java
@@ -0,0 +1,25 @@
+package com.example.springbooturlshortener.exception;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+  /* 400 BAD_REQUEST : 잘못된 요청 */
+  INVALID_URL(BAD_REQUEST, "잘못된 URL 정보입니다.");
+
+  private final HttpStatus httpStatus;
+  private final String detail;
+
+  ErrorCode(HttpStatus httpStatus, String detail) {
+    this.httpStatus = httpStatus;
+    this.detail = detail;
+  }
+
+  public HttpStatus getHttpStatus() {
+    return httpStatus;
+  }
+
+  public String getDetail() {
+    return detail;
+  }
+}
