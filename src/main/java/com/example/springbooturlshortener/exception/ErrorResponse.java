301d21d48db6272aedf1a9abb8d3cecd6e4ae867
hayoung <>
2022-06-18 11:16:22 +0900

    feat: 예외 처리 구현

---
+++ src/main/java/com/example/springbooturlshortener/exception/ErrorResponse.java
@@ -0,0 +1,25 @@
+package com.example.springbooturlshortener.exception;
+
+import java.time.LocalDateTime;
+import org.springframework.http.ResponseEntity;
+
+public class ErrorResponse {
+
+  private LocalDateTime timestamp = LocalDateTime.now();
+  private String error;
+  private String message;
+
+  private ErrorResponse(String error, String message) {
+    this.error = error;
+    this.message = message;
+  }
+
+  public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+    return ResponseEntity.status(errorCode.getHttpStatus())
+                         .body(
+                           new ErrorResponse(
+                             errorCode.getHttpStatus().name(),
+                             errorCode.getDetail())
+                         );
+  }
+}
