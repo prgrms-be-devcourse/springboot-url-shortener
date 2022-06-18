e02fd268e167c095ba75b45a87003be08d68f4e7
hayoung <>
2022-06-18 11:16:48 +0900

    feat: 예외 처리 구현

---
+++ src/main/java/com/example/springbooturlshortener/exception/CustomException.java
@@ -0,0 +1,18 @@
+package com.example.springbooturlshortener.exception;
+
+public class CustomException extends RuntimeException {
+
+  private final ErrorCode errorCode;
+
+  public CustomException(ErrorCode errorCode) {
+    this.errorCode = errorCode;
+  }
+
+  public String getMessage() {
+    return errorCode.getDetail();
+  }
+
+  public ErrorCode getErrorCode() {
+    return errorCode;
+  }
+}
