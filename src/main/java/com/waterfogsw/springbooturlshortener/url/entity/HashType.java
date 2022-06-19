f67bb6ab4d4a198c3d27b93e90c743eba528ba65
San Kim <waterfog9580@gmail.com>
2022-06-19 17:49:28 +0900

    :sparkles: HashGenerator 구현

--- src/main/java/com/waterfogsw/springbooturlshortener/url/entity/HashType.java
+++ src/main/java/com/waterfogsw/springbooturlshortener/url/entity/HashType.java
@@ -1,6 +1,16 @@
 package com.waterfogsw.springbooturlshortener.url.entity;
 
 public enum HashType {
-  MD5,
-  SHA256
+  MD5(32),
+  SHA256(64);
+
+  private final int maxLength;
+
+  HashType(int maxLength) {
+    this.maxLength = maxLength;
+  }
+
+  public int getMaxLength() {
+    return maxLength;
+  }
 }
