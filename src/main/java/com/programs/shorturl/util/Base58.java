c9beaf42bf896813f5efc5c9182e060fc9f25b4a
unnokid <kimki0609@gmail.com>
2022-06-20 02:38:08 +0900

    Add Base58

---
+++ src/main/java/com/programs/shorturl/util/Base58.java
@@ -0,0 +1,22 @@
+package com.programs.shorturl.util;
+
+import java.util.Random;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Base58 {
+
+  private static final char[] list = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz".toCharArray();
+
+  public String GenerateShortUrl() {
+
+    StringBuilder sb = new StringBuilder();
+    Random random = new Random();
+    for (int i = 0; i < 7; i++) {
+      int index = random.nextInt(58);
+      sb.append(list[index]);
+    }
+    return sb.toString();
+  }
+
+}
