6f1a9ff4dd9e986122b81a12c63f4d54c71cf8fd
hayoung <>
2022-06-19 22:04:11 +0900

    fix: UrlRepository 파라미터 순서 수정

--- src/main/java/com/example/springbooturlshortener/domain/UrlRepository.java
+++ src/main/java/com/example/springbooturlshortener/domain/UrlRepository.java
@@ -3,7 +3,7 @@ package com.example.springbooturlshortener.domain;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UrlRepository extends JpaRepository<Long, Url> {
+public interface UrlRepository extends JpaRepository<Url, Long> {
 
   Long save(Url url);
 
