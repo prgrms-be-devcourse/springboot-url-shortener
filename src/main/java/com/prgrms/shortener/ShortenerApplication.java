8e4386d41260146e6bb60e771aa9717ff3bb62c1
UJ15 <dbwogml15@naver.com>
2022-06-19 17:49:04 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/prgrms/shortener/ShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prgrms/shortener/ShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
