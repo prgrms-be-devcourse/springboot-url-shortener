81e4c351e2997fdd1865b25858aedadd82b28851
UJ15 <dbwogml15@naver.com>
2022-06-23 07:23:31 +0900

    :sparkles: decoding 메서드 삭제

--- src/main/java/com/prgrms/shortener/util/EncoderUtil.java
+++ src/main/java/com/prgrms/shortener/util/EncoderUtil.java
@@ -19,18 +19,4 @@ public class EncoderUtil {
 
     return sb.toString();
   }
-
-  // public Long decoding(String shortKey) {
-  //
-  //   long urlId = 0L;
-  //   int power = 1;
-  //
-  //   for (int i = 0 ; i < shortKey.length() ; i++) {
-  //    int index = BASE62.indexOf(shortKey.charAt(i));
-  //    urlId += (long) index * power;
-  //    power *= BASE62.length();
-  //   }
-  //
-  //   return urlId;
-  // }
 }
