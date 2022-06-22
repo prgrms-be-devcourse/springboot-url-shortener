964f56d411c4aca938234fc5db3c341ffc114921
unnokid <kimki0609@gmail.com>
2022-06-23 04:15:50 +0900

    Add Dto

---
+++ src/main/java/com/programs/shorturl/dto/UrlRequestDto.java
@@ -0,0 +1,12 @@
+package com.programs.shorturl.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UrlRequestDto {
+
+  private String originalUrl;
+
+}
