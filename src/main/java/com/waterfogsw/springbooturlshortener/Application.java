03d9727fc4f3fdb6d243a76a076b2a994977d25a
San Kim <waterfog9580@gmail.com>
2022-06-23 13:49:46 +0900

    :adhesive_bandage: AppProperties Constructor Binding 으로 변경

--- src/main/java/com/waterfogsw/springbooturlshortener/Application.java
+++ src/main/java/com/waterfogsw/springbooturlshortener/Application.java
@@ -3,6 +3,7 @@ package com.waterfogsw.springbooturlshortener;
 import com.waterfogsw.springbooturlshortener.common.properties.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableConfigurationProperties({
     AppProperties.class
 })
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class Application {
 
--- src/main/java/com/waterfogsw/springbooturlshortener/common/properties/AppProperties.java
+++ src/main/java/com/waterfogsw/springbooturlshortener/common/properties/AppProperties.java
@@ -2,20 +2,22 @@ package com.waterfogsw.springbooturlshortener.common.properties;
 
 import javax.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
+@ConstructorBinding
 @ConfigurationProperties("app")
 public class AppProperties {
 
   @NotEmpty
-  private String url;
+  private final String url;
 
   public String getUrl() {
     return url;
   }
 
-  public void setUrl(String url) {
+  public AppProperties(String url) {
     this.url = url;
   }
 }
