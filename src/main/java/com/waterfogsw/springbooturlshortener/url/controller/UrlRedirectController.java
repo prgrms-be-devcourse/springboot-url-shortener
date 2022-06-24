38c47094a8d5981d82e926763237ec4db1f47043
San Kim <waterfog9580@gmail.com>
2022-06-25 08:28:44 +0900

    :adhesive_bandage: redirect 메서드 IOException 발생시 런타임 에러 발생시키도록 수정

--- src/main/java/com/waterfogsw/springbooturlshortener/url/controller/UrlRedirectController.java
+++ src/main/java/com/waterfogsw/springbooturlshortener/url/controller/UrlRedirectController.java
@@ -1,16 +1,17 @@
 package com.waterfogsw.springbooturlshortener.url.controller;
 
-import com.waterfogsw.springbooturlshortener.common.exception.NotFoundException;
-import com.waterfogsw.springbooturlshortener.url.serivce.UrlService;
 import java.io.IOException;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.NotEmpty;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.waterfogsw.springbooturlshortener.url.serivce.UrlService;
 
 @RestController
 public class UrlRedirectController {
@@ -33,6 +34,7 @@ public class UrlRedirectController {
       response.sendRedirect(redirectUri);
     } catch (IOException e) {
       log.warn(e.getMessage());
+      throw new IllegalStateException(e.getMessage());
     }
   }
 }
