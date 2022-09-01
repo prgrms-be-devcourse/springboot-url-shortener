7240999a4b6629885b81c3120dc8ef301578d782
Gosh95 <goshk1995@gmail.com>
2022-09-02 01:08:59 +0900

    refactor: URL 벨리데이션 기능을 제거하라

--- src/main/java/prgrms/project/shorturl/dto/ShortUrlCreateRequest.java
+++ src/main/java/prgrms/project/shorturl/dto/ShortUrlCreateRequest.java
@@ -2,10 +2,7 @@ package prgrms.project.shorturl.dto;
 
 import javax.validation.constraints.NotBlank;
 
-import prgrms.project.shorturl.global.validation.UrlChecker;
-
 public record ShortUrlCreateRequest(
-	@UrlChecker(min = 4, max = 500)
 	String originUrl,
 
 	@NotBlank
