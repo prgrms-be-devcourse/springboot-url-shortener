81a9f9638cf9c252bdc2a7ec3561ba0e439323b3
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-31 21:49:37 +0900

    feat : initial commit

---
+++ src/main/java/org/prgrms/url/shortner/springbooturlshortner/SpringbootUrlShortnerApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.url.shortner.springbooturlshortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUrlShortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUrlShortnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/prgrms/url/shortner/springbooturlshortner/SpringbootUrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgrms.url.shortner.springbooturlshortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
