37701d124d47d3b78fbba9e682ce15b3d7cfc796
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-01-02 06:38:00 +0900

    feat : Url entity 수정

--- src/main/java/org/prgrms/url/shortner/springbooturlshortner/domain/Url.java
+++ src/main/java/org/prgrms/url/shortner/springbooturlshortner/domain/Url.java
@@ -6,22 +6,32 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Url {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "shorten_url",nullable = false)
-	private String shortenUrl;
+	@Column(name = "shorten_url", nullable = false, unique = true)
+	private String shortenKey;
 
 	@Column(name = "origin_url",nullable = false)
 	private String originUrl;
 
 	@Column(name = "view_count",nullable = false)
 	private int viewCount;
+
+	@Builder
+	public Url(String shortenKey, String originUrl) {
+		this.shortenKey = shortenKey;
+		this.originUrl = originUrl;
+	}
 }
