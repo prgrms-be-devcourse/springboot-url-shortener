3e85ba3c3c44ec8ff6e733e0edc5d0a6258b204b
thguss <thgus345@naver.com>
2023-01-03 00:47:11 +0900

    [FEAT] shortener 생성

---
+++ src/main/java/com/example/urlshortenert2/service/ShortenerService.java
@@ -0,0 +1,12 @@
+package com.example.urlshortenert2.service;
+
+import com.example.urlshortenert2.dto.ShortenerRequestDto;
+import com.example.urlshortenert2.dto.ShortenerResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShortenerService {
+    public ShortenerResponseDto createShortener(ShortenerRequestDto dto) {
+        return null;
+    }
+}
