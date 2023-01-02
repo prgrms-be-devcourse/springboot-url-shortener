537483f816687abc7052d45e7a733d95f5f297b9
박세준 <tpwns6693@gmail.com>
2023-01-02 23:55:00 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/example/urlshortenert2/UrlShortenerT2Application.java
@@ -0,0 +1,13 @@
+package com.example.urlshortenert2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerT2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerT2Application.class, args);
+    }
+
+}
