cdbb82900e6f4da3d75acbaad6755553c6c28ee4
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-01-03 01:08:48 +0900

    feat: ShortenerController - short URL 리다이렉션 메서드 구현

--- src/main/java/com/example/urlshortenert2/controller/ShortenerController.java
+++ src/main/java/com/example/urlshortenert2/controller/ShortenerController.java
@@ -3,14 +3,14 @@ package com.example.urlshortenert2.controller;
 import com.example.urlshortenert2.dto.ShortenerRequestDto;
 import com.example.urlshortenert2.dto.ShortenerResponseDto;
 import com.example.urlshortenert2.service.ShortenerService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
-@RequestMapping("/api/v1/shortener")
 public class ShortenerController {
 
     private final ShortenerService shortenerService;
@@ -20,13 +20,20 @@ public class ShortenerController {
     }
 
     // TODO: POST shortener 생성
-    @PostMapping
+    @PostMapping("/api/v1/shortener")
     public ResponseEntity<ShortenerResponseDto> createShortener(@RequestBody ShortenerRequestDto dto) {
         ShortenerResponseDto shortenerUrl = shortenerService.createShortener(dto);
         return ResponseEntity.ok().body(shortenerUrl);
     }
 
     // TODO: GET 리다이렉트 URL 제공
+    @GetMapping("/{randomId}")
+    public ResponseEntity<Object> getRealURL(@PathVariable String randomId) {
+        String realUrl = shortenerService.findById(randomId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create(realUrl));
+        return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY); // 301
+    }
 
     // TODO: GET 입력폼 HTML 제공
 }
--- src/main/java/com/example/urlshortenert2/service/ShortenerService.java
+++ src/main/java/com/example/urlshortenert2/service/ShortenerService.java
@@ -6,7 +6,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ShortenerService {
+
+
+
     public ShortenerResponseDto createShortener(ShortenerRequestDto dto) {
         return null;
+    }
+
+    public String findById(String randomId) {
+        return null; // 진짜 url 반환
     }
 }
