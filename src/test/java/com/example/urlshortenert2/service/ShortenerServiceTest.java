1d78a568ff7a2b7e731c87a1cd092f55adb5ab4e
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-01-03 15:40:10 +0900

    feat: service 테스트를 추가하다.

---
+++ src/test/java/com/example/urlshortenert2/service/ShortenerServiceTest.java
@@ -0,0 +1,61 @@
+package com.example.urlshortenert2.service;
+
+import com.example.urlshortenert2.ShorteningKeyMaker.ShorteningKeyMaker;
+import com.example.urlshortenert2.dto.ShortenerRequestDto;
+import com.example.urlshortenert2.dto.ShortenerResponseDto;
+import com.example.urlshortenert2.model.ShortedUrl;
+import com.example.urlshortenert2.repository.ShortedUrlRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@Slf4j
+@ExtendWith(MockitoExtension.class)
+class ShortenerServiceTest {
+
+    @Mock
+    private ShortedUrlRepository repository;
+    @Mock
+    private ShorteningKeyMaker maker;
+
+    @InjectMocks
+    private ShortenerService shortenerService;
+
+    @Test
+    @DisplayName("짧은 url을 생성할 수 있다.")
+    void short_url_생성테스트() {
+        // given
+        ShortenerRequestDto request = new ShortenerRequestDto("www.naver.com/dltndud");
+        ShortedUrl shortedUrl = new ShortedUrl(request.url());
+        // when
+        when(repository.save(shortedUrl)).thenReturn(shortedUrl);
+        when(maker.makeShorteningKey(shortedUrl.getId())).thenReturn("1");
+        ShortenerResponseDto response = shortenerService.createShortener(request);
+        // then
+        verify(repository).save(shortedUrl);
+        verify(maker).makeShorteningKey(anyLong());
+        Assertions.assertThat(response.shorteningKey()).isEqualTo("1");
+    }
+
+    @Test
+    @DisplayName("shortening key를 이용해서 특정 기존 진짜 URL을 찾을 수 있다.")
+    void shortening_key_조회테스트() {
+        // given
+        ShortedUrl responseUrl = new ShortedUrl("www.naver.com/dltndud");
+        when(repository.findShortedUrlByShorteningKey(anyString())).thenReturn(Optional.of(responseUrl));
+        // when
+        String originUrl = shortenerService.findById(anyString());
+        // then
+        verify(repository).findShortedUrlByShorteningKey(anyString());
+        Assertions.assertThat(originUrl).isEqualTo(responseUrl.getOriginUrl());
+    }
+}
