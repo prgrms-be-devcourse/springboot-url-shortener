f47630954b43113d2f48d67ff9383d7fd7dac0b1
Kong <rhdtn311@naver.com>
2023-01-03 23:21:04 +0900

    refactor : 잘못된 URL 입력 시 사용자 화면에 표시해줌

--- src/main/java/com/programmers/springbooturlshortener/web/UrlController.java
+++ src/main/java/com/programmers/springbooturlshortener/web/UrlController.java
@@ -1,23 +1,17 @@
 package com.programmers.springbooturlshortener.web;
 
-import javax.persistence.EntityNotFoundException;
-import javax.validation.Valid;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.programmers.springbooturlshortener.domain.url.UrlService;
 import com.programmers.springbooturlshortener.domain.url.dto.UrlCreateDto;
 import com.programmers.springbooturlshortener.domain.url.dto.UrlResponseDto;
 import com.programmers.springbooturlshortener.domain.url.dto.UrlServiceRequestDto;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,7 +21,7 @@ public class UrlController {
 	private static final String URL_PREFIX = "localhost:8080/";
 
 	@GetMapping("/")
-	public String homePage() {
+	public String homePage(@ModelAttribute UrlCreateDto urlCreateDto) {
 		return "index";
 	}
 
