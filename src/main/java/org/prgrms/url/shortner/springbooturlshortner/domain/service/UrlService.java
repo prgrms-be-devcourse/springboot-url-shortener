08ab32dbdd2807d771ce9abe985e6c87da2aab27
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-01-04 23:26:55 +0900

    refactor : transactional 추가

--- src/main/java/org/prgrms/url/shortner/springbooturlshortner/domain/service/UrlService.java
+++ src/main/java/org/prgrms/url/shortner/springbooturlshortner/domain/service/UrlService.java
@@ -11,17 +11,20 @@ import org.prgrms.url.shortner.springbooturlshortner.domain.service.converter.Ur
 import org.prgrms.url.shortner.springbooturlshortner.global.error.exception.EntityNotFoundException;
 import org.prgrms.url.shortner.springbooturlshortner.global.error.exception.MaxAttemptException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UrlService {
 
 	private static final int MAX_ATTEMPT = 5;
 	private final UrlJpaRepository urlJpaRepository;
 	private final UrlConverter urlConverter;
 
+	@Transactional
 	public CreateResponse create(CreateRequest createRequest) {
 		Url url = urlConverter.getUrl(createRequest);
 		executeAlgorithm(url);
