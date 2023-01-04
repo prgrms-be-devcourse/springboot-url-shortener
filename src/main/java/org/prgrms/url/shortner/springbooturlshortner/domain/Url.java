f452c3c8d4c607a69ac049767b5fd148eb41748a
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-01-04 23:05:21 +0900

    refactor : Url Entity 수정

--- src/main/java/org/prgrms/url/shortner/springbooturlshortner/domain/Url.java
+++ src/main/java/org/prgrms/url/shortner/springbooturlshortner/domain/Url.java
@@ -1,10 +1,16 @@
 package org.prgrms.url.shortner.springbooturlshortner.domain;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
+
+import org.prgrms.url.shortner.springbooturlshortner.domain.algorithm.ShortUrlAlgorithm;
+import org.springframework.util.StringUtils;
 
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -21,17 +27,39 @@ public class Url {
 	private Long id;
 
 	@Column(name = "shorten_url", nullable = false, unique = true)
-	private String shortenKey;
+	private String shortenUrl;
 
-	@Column(name = "origin_url",nullable = false)
+	@Column(name = "origin_url", nullable = false)
 	private String originUrl;
 
-	@Column(name = "view_count",nullable = false)
-	private int viewCount;
+	@Transient
+	private ShortUrlAlgorithm shortUrlAlgorithm;
 
 	@Builder
-	public Url(String shortenKey, String originUrl) {
-		this.shortenKey = shortenKey;
+	public Url(String originUrl, ShortUrlAlgorithm shortUrlAlgorithm) {
+		validateOriginUrl(originUrl);
+		validateShortUrlAlgorithm(shortUrlAlgorithm);
 		this.originUrl = originUrl;
+		this.shortUrlAlgorithm = shortUrlAlgorithm;
+	}
+
+	private void validateOriginUrl(String originUrl) {
+		if (!StringUtils.hasText(originUrl)) {
+			throw new IllegalArgumentException("올바른 Url이 아닙니다.");
+		}
+	}
+
+	private void validateShortUrlAlgorithm(ShortUrlAlgorithm shortUrlAlgorithm) {
+		if (!Objects.nonNull(shortUrlAlgorithm)) {
+			throw new IllegalArgumentException("단축 알고리즘은 Null일 수 없습니다.");
+		}
+	}
+
+	public void allocateShortenUrl(String shortenUrl) {
+		this.shortenUrl = shortenUrl;
+	}
+
+	public String createShortenUrl() {
+		return shortUrlAlgorithm.execute(this.originUrl);
 	}
 }
