59d32ab4f44b72c4d15e36240a4c55c938a3ff7b
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-01-04 23:07:19 +0900

    feat : shortenUrl로 조회 기능 추가

--- src/main/java/org/prgrms/url/shortner/springbooturlshortner/domain/repository/UrlJpaRepository.java
+++ src/main/java/org/prgrms/url/shortner/springbooturlshortner/domain/repository/UrlJpaRepository.java
@@ -1,7 +1,13 @@
 package org.prgrms.url.shortner.springbooturlshortner.domain.repository;
 
+import java.util.Optional;
+
 import org.prgrms.url.shortner.springbooturlshortner.domain.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UrlJpaRepository extends JpaRepository<Url, Long> {
+
+	Optional<Url> findUrlByShortenUrlEquals(@Param("shortenUrl") String shortenUrl);
+
 }
