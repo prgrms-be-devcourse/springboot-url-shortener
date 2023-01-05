43d11b1a0215462e66d94b1779776e2421c5785f
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-01-05 16:41:58 +0900

    fix: 설정과 TODO를 추가하다.

--- src/main/java/com/example/urlshortenert2/ShorteningKeyMaker/Base62LibraryKeyMaker.java
+++ src/main/java/com/example/urlshortenert2/ShorteningKeyMaker/Base62LibraryKeyMaker.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 public class Base62LibraryKeyMaker implements ShorteningKeyMaker{
     @Override
     public String makeShorteningKey(Long id) {
+        //TODO: 여기 로직을 라이브러리 써서 하는게 생각해봐야 할 듯 => 키가 너무 짧다.
         Base62 base62 = Base62.createInstance();
         byte[] encoded = base62.encode(id.toString().getBytes());
         return new String(encoded);
