cda94bc5efd7529d09499e8e012b9ed00b33703b
thguss <thgus345@naver.com>
2023-01-06 14:39:56 +0900

    [FIX] BASE62 리팩토링 및 구현

--- src/main/java/com/example/urlshortenert2/ShorteningKeyMaker/Base62LibraryKeyMaker.java
+++ src/main/java/com/example/urlshortenert2/ShorteningKeyMaker/Base62LibraryKeyMaker.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
-public class Base62LibraryKeyMaker implements ShorteningKeyMaker{
+public class Base62LibraryKeyMaker implements ShorteningKeyMaker {
+
     @Override
     public String makeShorteningKey(Long id) {
         //TODO: 여기 로직을 라이브러리 써서 하는게 생각해봐야 할 듯 => 키가 너무 짧다.
--- src/main/java/com/example/urlshortenert2/ShorteningKeyMaker/MyShorteningKeyMaker.java
+++ src/main/java/com/example/urlshortenert2/ShorteningKeyMaker/MyShorteningKeyMaker.java
@@ -1,12 +1,35 @@
 package com.example.urlshortenert2.ShorteningKeyMaker;
 
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class MyShorteningKeyMaker implements ShorteningKeyMaker {
+
+    private static final char[] BASE_62_CHAR_ARRAY = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+    private static final int BASE_62_LENGTH = BASE_62_CHAR_ARRAY.length;
+    private static final int SHORT_URL_LENGTH = 8;
+    private static final char PADDING_CHARACTER = '+';
+
     @Override
     public String makeShorteningKey(Long id) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        while(id > 0) {
+            int idx = (int)(id % BASE_62_LENGTH);
+            sb.append(BASE_62_CHAR_ARRAY[idx]);
+            id /= BASE_62_LENGTH;
+        }
+        return padding(sb);
+    }
+
+    private String padding(StringBuilder sb) {
+        if (sb.length() >= SHORT_URL_LENGTH) {
+            return sb.toString();
+        }
+        while (sb.length() < SHORT_URL_LENGTH) {
+            sb.append(PADDING_CHARACTER);
+        }
+        return sb.toString();
     }
 }
