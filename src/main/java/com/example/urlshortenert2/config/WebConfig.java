3e3627eb501b5cf640c7a936871d26970ed44464
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-01-09 20:30:21 +0900

    refactor: CORS 설정을 파일로 분리시키고 allowedOrgins의 범위를 좁히다.

---
+++ src/main/java/com/example/urlshortenert2/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.urlshortenert2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods(GET.name(), POST.name());
+    }
+}
--- src/main/java/com/example/urlshortenert2/controller/ShortenerApiController.java
+++ src/main/java/com/example/urlshortenert2/controller/ShortenerApiController.java
@@ -8,41 +8,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
-import java.net.*;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ShortenerApiController {
 
     private final ShortenerService shortenerService;
 
     public ShortenerApiController(ShortenerService shortenerService) {
         this.shortenerService = shortenerService;
-    }
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Object> notFoundHandler() {
-        return ResponseEntity.notFound().build();
-    }
-
-    @ExceptionHandler(MalformedURLException.class)
-    public ResponseEntity<Object> illegalUrlHandler() {
-        return ResponseEntity.badRequest()
-                .body("잘못된 URL 형식입니다.(http://, https:// 를 반드시 넣어 주어야 합니다.)");
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Object> notConnectionUrlHandler(IllegalArgumentException exception) {
-        return ResponseEntity.badRequest()
-                .body(exception.getMessage());
-    }
-
-    @ExceptionHandler(UnknownHostException.class)
-    public ResponseEntity<Object> notPresentDomainUrlHandler() {
-        return ResponseEntity.badRequest()
-                .body("존재하지 않는 도메인의 URL 입니다.");
     }
 
     @PostMapping("/api/v1/shortener")
