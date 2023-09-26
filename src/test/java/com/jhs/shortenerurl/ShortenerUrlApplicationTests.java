2b9aad4bfa94afa8adf5a73293c9d13c0c2b8da7
HongSeok Ju <wnghdtjr129@naver.com>
2023-09-26 12:53:15 +0900

    feat: project setting

---
+++ src/test/java/com/jhs/shortenerurl/ShortenerUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhs.shortenerurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
