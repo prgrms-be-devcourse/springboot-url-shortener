309cae3da0ee6c04f86c45efd91c284371377de7
JoJaeHyeon <joje1003@naver.com>
2023-09-27 16:47:08 +0900

    feat: service, repository 구현

--- shortUrl/src/main/java/com/urlMaker/shortUrl/UrlRepository.java
+++ shortUrl/src/main/java/com/urlMaker/shortUrl/UrlRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UrlRepository extends JpaRepository<Url,Long> {
-    Optional<Url> findByShortenUrl(String shortenUrl);
+    Optional<Url> findByOriginUrl(String originUrl);
 }
--- shortUrl/src/main/java/com/urlMaker/shortUrl/UrlService.java
+++ shortUrl/src/main/java/com/urlMaker/shortUrl/UrlService.java
@@ -1,14 +1,57 @@
 package com.urlMaker.shortUrl;
 
+import com.urlMaker.shortUrl.algorithm.Algorithm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UrlService {
 
     private final UrlRepository urlRepository;
+    private final Algorithm algorithm;
 
+    @Transactional
+    public UrlCreateResponseDTO createShortenUrl(UrlCreateRequestDTO urlCreateRequestDTO) {
+        Url url = urlCreateRequestDTO.toEntity();
 
+        Optional<Url> retrievedURL = urlRepository.findByOriginUrl(url.getOriginUrl());
 
+        if(retrievedURL.isPresent()){
+            retrievedURL.get().increaseRequestCount();
+
+            return UrlCreateResponseDTO.builder()
+                    .shortenUrl(algorithm.encode(url.getUrlId()))
+                    .build();
+        }
+
+        Url savedUrl = urlRepository.save(url);
+        String encodedShortenUrl = algorithm.encode(savedUrl.getUrlId());
+
+        return UrlCreateResponseDTO.builder()
+                .shortenUrl(encodedShortenUrl)
+                .requestCount(url.getRequestCount())
+                .build();
+    }
+
+    @Transactional(readOnly = true)
+    public UrlResponseDTO getOriginUrl(String shortenUrl){
+
+        Long urlId = algorithm.decode(shortenUrl);
+
+        Url originUrl = urlRepository.findById(urlId)
+                .orElseThrow(() -> {
+                    throw new IllegalArgumentException("no url");
+                });
+
+        return UrlResponseDTO.builder()
+                .originUrl(originUrl.getOriginUrl())
+                .requestCount(originUrl.getRequestCount())
+                .build();
+    }
 }
