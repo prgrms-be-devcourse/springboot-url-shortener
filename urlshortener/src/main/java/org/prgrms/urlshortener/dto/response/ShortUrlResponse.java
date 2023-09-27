84cf25e2a47eff67a000f05cbce5d8946c502363
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-09-27 16:46:30 +0900

    refactor : ShortUrlReponse 인자개수 변경

--- urlshortener/src/main/java/org/prgrms/urlshortener/dto/response/ShortUrlResponse.java
+++ urlshortener/src/main/java/org/prgrms/urlshortener/dto/response/ShortUrlResponse.java
@@ -3,13 +3,22 @@ package org.prgrms.urlshortener.dto.response;
 import org.prgrms.urlshortener.domain.Algorithm;
 import org.prgrms.urlshortener.domain.Url;
 
+import lombok.Builder;
+
+@Builder
 public record ShortUrlResponse(
+	String originUrl,
 	String shortUrl,
 	Algorithm algorithm,
 	int hitCount
 ) {
 
 	public static ShortUrlResponse from(Url url) {
-		return new ShortUrlResponse(url.getShortUrl(), url.getAlgorithm(), url.getHitCount());
+		return ShortUrlResponse.builder()
+			.originUrl(url.getOriginUrl())
+			.shortUrl(url.getShortUrl())
+			.algorithm(url.getAlgorithm())
+			.hitCount(url.getHitCount())
+			.build();
 	}
 }
