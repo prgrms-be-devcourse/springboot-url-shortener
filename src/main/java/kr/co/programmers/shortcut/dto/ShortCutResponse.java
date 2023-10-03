453547f99c7c325678d8962c284ed219e0cf6801
Curry4182 <kangbk4182@naver.com>
2023-10-04 01:38:39 +0900

    feat: shortcut 반환 dto 생성

--- src/main/java/kr/co/programmers/shortcut/dto/ShortCutResponse.java
+++ src/main/java/kr/co/programmers/shortcut/dto/ShortCutResponse.java
@@ -1,7 +1,16 @@
 package kr.co.programmers.shortcut.dto;
 
+import kr.co.programmers.shortcut.domain.ShortCut;
+
 public record ShortCutResponse (
 	String originalURL,
 	String newURL
 ) {
+
+	public static ShortCutResponse from(ShortCut shortCut) {
+		return new ShortCutResponse(
+			shortCut.getOriginalURL(),
+			shortCut.getNewURL()
+		);
+	}
 }
