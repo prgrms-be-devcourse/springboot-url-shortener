9dd84c6d2cdde9a4be365eab5239e9ffa1b216b1
Yiseul Park <omjl5123@catholic.ac.kr>
2023-10-03 16:11:33 +0900

    feat: URL Click Counter 기능 추가

--- src/main/java/com/programmers/urlshortener/url/controller/UrlController.java
+++ src/main/java/com/programmers/urlshortener/url/controller/UrlController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.programmers.urlshortener.url.dto.UrlShortenRequest;
@@ -51,5 +53,18 @@ public class UrlController {
 		model.addAttribute("totalClicks", totalClicks);
 
 		return "totalUrlClicks";
+	}
+
+	@GetMapping("/total-url-clicks")
+	public String UrlClickCounter() {
+		return "UrlClickCounter";
+	}
+
+	@PostMapping("/total-url-clicks")
+	public String redirectTotalClicks(@RequestParam String shorteningKey, RedirectAttributes redirectAttributes) {
+		log.info("shorteningKey={}", shorteningKey);
+		redirectAttributes.addAttribute("shorteningKey", shorteningKey);
+
+		return "redirect:/total-url-clicks/{shorteningKey}";
 	}
 }
