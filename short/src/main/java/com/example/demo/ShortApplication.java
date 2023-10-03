13db68e3eabb596d09c90e21aef167f8a9ca735e
hi-june <wnsgur735@gmail.com>
2023-10-03 16:42:38 +0900

    initial commit

---
+++ short/src/main/java/com/example/demo/ShortApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortApplication.class, args);
+	}
+
+}
