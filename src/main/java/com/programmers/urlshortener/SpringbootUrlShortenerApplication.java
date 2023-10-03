62336384365d72e879a5819cb2818d0a7b6f4dc9
changhyeon <changhyeon.h@kakao.com>
2023-10-03 16:47:24 +0900

    feat: JpaAudting 기능 추가

--- src/main/java/com/programmers/urlshortener/SpringbootUrlShortenerApplication.java
+++ src/main/java/com/programmers/urlshortener/SpringbootUrlShortenerApplication.java
@@ -2,8 +2,10 @@ package com.programmers.urlshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringbootUrlShortenerApplication {
 
     public static void main(String[] args) {
