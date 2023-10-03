413974e5d00b4f87c1cf92748b24fb0c323bba9c
changhyeon <changhyeon.h@kakao.com>
2023-10-03 23:45:18 +0900

    style: 개행 삭제

--- src/main/java/com/programmers/urlshortener/SpringbootUrlShortenerApplication.java
+++ src/main/java/com/programmers/urlshortener/SpringbootUrlShortenerApplication.java
@@ -11,5 +11,4 @@ public class SpringbootUrlShortenerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootUrlShortenerApplication.class, args);
     }
-
 }
