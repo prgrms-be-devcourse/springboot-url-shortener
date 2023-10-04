b00daee691ed5a634f16a0f69ee73cdf2de5e3ce
Curry4182 <kangbk4182@naver.com>
2023-10-04 16:11:15 +0900

    feat: 원본 URL을 입력 받아 shortcut URL를 생성하는 기능 추가

---
+++ src/main/java/kr/co/programmers/shortcut/application/ShortCutService.java
@@ -0,0 +1,46 @@
+package kr.co.programmers.shortcut.application;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.co.programmers.shortcut.domain.ShortCut;
+import kr.co.programmers.shortcut.domain.ShortCutRepository;
+import kr.co.programmers.shortcut.dto.ShortCutCreateRequest;
+import kr.co.programmers.shortcut.dto.ShortCutResponse;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ShortCutService {
+
+	private final ShortCutRepository shortCutRepository;
+
+	private final ShortCutGenerator shortCutGenerator;
+
+	private final Base62 base62;
+
+	@Transactional
+	public ShortCutResponse createShortCut(ShortCutCreateRequest shortCutCreateRequest) {
+		String originalURL = shortCutCreateRequest.originalURL();
+		ShortCut shortCut = new ShortCut(originalURL);
+
+		ShortCut savedShortCut = shortCutRepository.save(shortCut);
+		Long savedShortCutId = savedShortCut.getId();
+
+		String encodeId = base62.encodeNumber(savedShortCutId);
+
+		savedShortCut.updateEncodedId(encodeId);
+		shortCutRepository.save(savedShortCut);
+
+		String shortCutURL = shortCutGenerator.generateShortCutURL(encodeId);
+
+		return ShortCutResponse.from(savedShortCut, shortCutURL);
+	}
+
+	public ShortCutResponse getShortCutByEncodedId(String encodedId, String shortCutURL) {
+		Long decodedId = base62.decodeKey(encodedId);
+		ShortCut findedShortCut = shortCutRepository.getReferenceById(decodedId);
+		return ShortCutResponse.from(findedShortCut, shortCutURL);
+	}
+}
