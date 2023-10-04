c6d05ed1b09b12c7cef6d439887ffb4364ee2d21
Curry4182 <kangbk4182@naver.com>
2023-10-04 16:13:43 +0900

    feat: 원본 URL 입력 페이지와 shortcut 결과 페이지 반환 기능 추가

--- src/main/java/kr/co/programmers/shortcut/api/ShortCutMainController.java
+++ src/main/java/kr/co/programmers/shortcut/api/ShortCutMainController.java
@@ -1,11 +1,35 @@
 package kr.co.programmers.shortcut.api;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import kr.co.programmers.shortcut.application.ShortCutService;
+import kr.co.programmers.shortcut.dto.ShortCutCreateRequest;
+import kr.co.programmers.shortcut.dto.ShortCutResponse;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/shortcut")
 public class ShortCutMainController {
 
-	@GetMapping("/index")
-	public String getIndex() {
-		return "index.html";
+	private final ShortCutService shortCutService;
+
+	@GetMapping()
+	public String showForm(Model model) {
+		model.addAttribute("shortCutCreateRequest", new ShortCutCreateRequest("input url"));
+		return "shortcut-form";
+	}
+
+	@PostMapping()
+	public ModelAndView submitForm(@Validated @ModelAttribute ShortCutCreateRequest shortCutCreateRequest) {
+		ShortCutResponse shortCutResponse = shortCutService.createShortCut(shortCutCreateRequest);
+		return new ModelAndView("shortcut-result", "shortCut", shortCutResponse);
 	}
 }
