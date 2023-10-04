b2c86146b98b3e8e1c356337165e284623d1ab47
HyuckJuneHong <jjj8337721@naver.com>
2023-10-04 20:24:51 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/example/kdtspringbooturlshortener/KdtSpringbootUrlShortenerApplication.java
@@ -0,0 +1,12 @@
+package com.example.kdtspringbooturlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtSpringbootUrlShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdtSpringbootUrlShortenerApplication.class, args);
+    }
+}
