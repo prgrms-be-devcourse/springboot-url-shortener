ef8212848753e95f9402a295b655acb6be35a3c6
ysng_ysng <yeasung67@gmail.com>
2023-10-04 15:01:12 +0900

    feat: setup springboot-url-shortener

---
+++ src/test/java/shortener/SpringbootUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
