ee6a8b61728ae68dac70381dec922d0e7a96dafb
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-10-06 20:16:29 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/programmers/springbooturlshortener/SpringbootUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.springbooturlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
