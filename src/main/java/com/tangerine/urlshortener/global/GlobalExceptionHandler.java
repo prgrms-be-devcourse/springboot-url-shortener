f32d336a488e95eaa6fbc23b5c091f16c389087d
Sey0 <onetuks@naver.com>
2023-10-06 18:14:44 +0900

    refactor: 타임리프 UI 수정

---
+++ src/main/java/com/tangerine/urlshortener/global/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.tangerine.urlshortener.global;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String illegalArgumentExceptionHandler(IllegalArgumentException e) {
+        logger.warn("{} - {}", getClass().getSimpleName(), e);
+        return "error/bad-request";
+    }
+
+    @ResponseStatus(NOT_FOUND)
+    @ExceptionHandler(RuntimeException.class)
+    public String runtimeExceptionHandler(RuntimeException e) {
+        logger.warn("{} - {}", getClass().getSimpleName(), e);
+        return "error/not-found";
+    }
+
+}
