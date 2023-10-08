c8f23bb6abbdd12ee47d130e39a6924af2b1d9e8
ysng_ysng <yeasung67@gmail.com>
2023-10-09 04:48:08 +0900

    feat: RestControllerAdvice 구현

--- src/main/java/shortener/global/error/ErrorCode.java
+++ src/main/java/shortener/global/error/ErrorCode.java
@@ -3,7 +3,8 @@ package shortener.global.error;
 public enum ErrorCode {
 	INVALID_REQUEST_NUMBERS("U001", "API의 총 요청 횟수가 잘 못 되었습니다."),
 	NOT_FOUND_MAPPED_URL("U002", "요청받은 URL을 찾을 수 없습니다."),
-	INVALID_ENCODING_ALGORITHM("U003", "올바르지 않은 인코딩 요청입니다.");
+	INVALID_ENCODING_ALGORITHM("U003", "올바르지 않은 인코딩 요청입니다."),
+	INVALID_INPUT_VALUE("U004", "올바르지 않은 입력입니다.");
 
 	private final String code;
 	private final String message;
--- src/main/java/shortener/global/error/GlobalExceptionHandler.java
+++ src/main/java/shortener/global/error/GlobalExceptionHandler.java
@@ -1,8 +1,35 @@
 package shortener.global.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import shortener.global.error.exception.BusinessException;
+import shortener.global.error.exception.EntityNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException e) {
+		ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
+
+		return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(BusinessException.class)
+	public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+		ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
+
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	}
 }
