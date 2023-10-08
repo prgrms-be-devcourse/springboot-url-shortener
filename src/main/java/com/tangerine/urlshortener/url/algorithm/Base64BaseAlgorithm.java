a2c16e3d3a59e54d034309ad5e6ba1d69fb551da
Sey0 <onetuks@naver.com>
2023-10-08 16:36:19 +0900

    refactor: 알고리즘 클래스에서 외부 변수명 제거

--- src/main/java/com/tangerine/urlshortener/url/algorithm/Base64BaseAlgorithm.java
+++ src/main/java/com/tangerine/urlshortener/url/algorithm/Base64BaseAlgorithm.java
@@ -8,24 +8,25 @@ public class Base64BaseAlgorithm implements BaseAlgorithm {
     private static final String PADDING_TOKEN = "A";
 
     @Override
-    public String encode(Long mappingId) {
+    public String encode(Long encodeValue) {
         StringBuilder sb = new StringBuilder();
-        while (mappingId > 0) {
-            sb.append(tokens[(int) (mappingId % TOKEN_LENGTH_BOUND)]);
-            mappingId /= TOKEN_LENGTH_BOUND;
+        Long value = encodeValue;
+        while (value > 0) {
+            sb.append(tokens[(int) (value % TOKEN_LENGTH_BOUND)]);
+            value /= TOKEN_LENGTH_BOUND;
         }
         return padding(sb);
     }
 
     @Override
-    public long decode(String shortUrl) {
-        long decodedId = 0L;
+    public long decode(String decodeValue) {
+        long value = 0L;
         long factor = 1L;
         for (int i = 0; i < URL_LENGTH_BOUND; i++) {
-            decodedId += String.valueOf(tokens).indexOf(shortUrl.charAt(i)) * factor;
+            value += String.valueOf(tokens).indexOf(decodeValue.charAt(i)) * factor;
             factor *= TOKEN_LENGTH_BOUND;
         }
-        return decodedId;
+        return value;
     }
 
     private String padding(StringBuilder sb) {
