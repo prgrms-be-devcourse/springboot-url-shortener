7f6f6dd38690bb116b89172d70932fa9f1b8cf51
wonu606 <wonu606@gmail.com>
2023-10-08 16:26:24 +0900

    fix: UrlLink 도메인에 검증 추가
    - 잘못된 id 값이 들어올 경우 Exception 발생하도록 수정

--- short-url/src/main/java/com/prgrms/wonu606/shorturl/domain/UrlLink.java
+++ short-url/src/main/java/com/prgrms/wonu606/shorturl/domain/UrlLink.java
@@ -12,6 +12,9 @@ public class UrlLink {
     }
 
     public void initializeId(Long newId) {
+        if (newId == null || newId <= 0) {
+            throw new IllegalArgumentException("ID는 0보다 크고 null이 아니어야 합니다. Input ID: " + newId);
+        }
         if (this.id != null) {
             throw new IllegalStateException("ID가 이미 할당되어 있습니다. Current ID: " + newId);
         }
@@ -20,5 +23,9 @@ public class UrlLink {
 
     public HashedShortUrl getHashedShortUrl() {
         return hashedShortUrl;
+    }
+
+    public long getId() {
+        return id;
     }
 }
