c66f64645c4bf533de3ca6d2b9d4d89004d32dd0
ysng_ysng <yeasung67@gmail.com>
2023-10-09 17:48:45 +0900

    feat: CORS 설정
    http://localhost:3000으로부터 오는 요청을 모두 허가했습니다.

---
+++ src/main/java/shortener/config/WebConfig.java
@@ -0,0 +1,19 @@
+package shortener.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:3000")
+			.allowedMethods("GET", "POST", "PUT", "DELETE")
+			.allowedHeaders("*");
+	}
+}
