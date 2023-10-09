46dfa03aa843752d9e57fd976646c285c1c73a27
Sey0 <onetuks@naver.com>
2023-10-09 18:09:48 +0900

    refactor: 문맥 따라 개행

--- src/main/java/com/tangerine/urlshortener/url/algorithm/Base64BaseAlgorithm.java
+++ src/main/java/com/tangerine/urlshortener/url/algorithm/Base64BaseAlgorithm.java
@@ -11,10 +11,12 @@ public class Base64BaseAlgorithm implements BaseAlgorithm {
     public String encode(Long encodeValue) {
         StringBuilder sb = new StringBuilder();
         Long value = encodeValue;
+
         while (value > 0) {
             sb.append(tokens[(int) (value % TOKEN_LENGTH_BOUND)]);
             value /= TOKEN_LENGTH_BOUND;
         }
+
         return padding(sb);
     }
 
@@ -22,19 +24,23 @@ public class Base64BaseAlgorithm implements BaseAlgorithm {
     public long decode(String decodeValue) {
         long value = 0L;
         long factor = 1L;
+
         for (int i = 0; i < URL_LENGTH_BOUND; i++) {
             value += String.valueOf(tokens).indexOf(decodeValue.charAt(i)) * factor;
             factor *= TOKEN_LENGTH_BOUND;
         }
+
         return value;
     }
 
     private String padding(StringBuilder sb) {
         int length = sb.length();
+
         while (length < URL_LENGTH_BOUND) {
             sb.append(PADDING_TOKEN);
             length++;
         }
+
         return sb.toString();
     }
 }
