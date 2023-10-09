3a5eaff108b39fe2075f263a2fd2641f3c4b4d81
wonu606 <wonu606@gmail.com>
2023-10-09 15:11:45 +0900

    test: `DefaultShortenerService` 테스트 코드 작성 및 통과

---
+++ short-url/src/test/java/com/prgrms/wonu606/shorturl/service/DefaultUrlShortenerServiceTest.java
@@ -0,0 +1,49 @@
+package com.prgrms.wonu606.shorturl.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.prgrms.wonu606.shorturl.domain.UrlHash;
+import com.prgrms.wonu606.shorturl.service.dto.ShortenUrlCreateParam;
+import com.prgrms.wonu606.shorturl.service.dto.ShortenUrlCreateResult;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultUrlShortenerServiceTest {
+
+    @Autowired
+    DefaultUrlShortenerService defaultUrlShortenerService;
+
+    @Autowired
+    UrlLinkRepository urlLinkRepository;
+
+    @Nested
+    class CreateShortenUrlHash {
+
+        @ParameterizedTest
+        @MethodSource("provideValidUrls")
+        void givenValidParam_CreatingUrlLink(ShortenUrlCreateParam param) {
+            // When
+            ShortenUrlCreateResult result = defaultUrlShortenerService.createShortenUrlHash(param);
+            UrlHash createdUrlHash = new UrlHash(result.hashedShortUrl());
+
+            // Then
+            assertThat(urlLinkRepository.existByUrlHash(createdUrlHash)).isTrue();
+        }
+
+        static Stream<Arguments> provideValidUrls() {
+            return Stream.of(
+                    Arguments.of(new ShortenUrlCreateParam("https://url.kr")),
+                    Arguments.of(new ShortenUrlCreateParam("https://www.stackoverflow.com")),
+                    Arguments.of(new ShortenUrlCreateParam("https://comic.naver.com/index")),
+                    Arguments.of(new ShortenUrlCreateParam("https://www.youtube.com/watch?v=Yc56NpYW1DM")),
+                    Arguments.of(new ShortenUrlCreateParam("https://www.youtube.com/@devbadak"))
+            );
+        }
+    }
+}
