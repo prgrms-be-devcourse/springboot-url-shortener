79620d8f3ca82159a7a9c482050c89a353d91a63
Sey0 <onetuks@naver.com>
2023-10-09 20:42:06 +0900

    refactor: POST 요청 body 방식으로 변경

--- src/main/java/com/tangerine/urlshortener/url/controller/UrlController.java
+++ src/main/java/com/tangerine/urlshortener/url/controller/UrlController.java
@@ -15,7 +15,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,7 +51,7 @@ public class UrlController {
      */
     @PostMapping(path = "/shortener", consumes = APPLICATION_FORM_URLENCODED_VALUE)
     public String shortenOriginUrl(
-            @ModelAttribute ShortenRequest shortenRequest,
+            ShortenRequest shortenRequest,
             Model model
     ) {
         UrlMappingResult mappingInfo = urlService.createShortUrl(shortenRequest.to());
