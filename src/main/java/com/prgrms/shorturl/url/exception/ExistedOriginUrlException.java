f42ba3bd7c19acf062400531ad8afc6cbf4e7a34
byeolhaha <j35635aa@gmail.com>
2023-10-10 12:21:57 +0900

    [Feat] : 커스텀 예외 및 관련 Handler 구현

---
+++ src/main/java/com/prgrms/shorturl/url/exception/ExistedOriginUrlException.java
@@ -0,0 +1,16 @@
+package com.prgrms.shorturl.url.exception;
+
+import com.prgrms.shorturl.global.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class ExistedOriginUrlException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public ExistedOriginUrlException(final ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+}
