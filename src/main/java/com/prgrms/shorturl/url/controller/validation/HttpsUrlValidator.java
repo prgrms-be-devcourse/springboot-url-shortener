3b704d519740805bf497a4fb7f229d9ac593ae86
byeolhaha <j35635aa@gmail.com>
2023-10-10 12:23:45 +0900

    [Feat] : Short Url Create Dto  및 custom 검증 어노테이션 구현

---
+++ src/main/java/com/prgrms/shorturl/url/controller/validation/HttpsUrlValidator.java
@@ -0,0 +1,16 @@
+package com.prgrms.shorturl.url.controller.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+
+public class HttpsUrlValidator implements ConstraintValidator<HttpUrl, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null || value.isBlank()) {
+            return true;
+        }
+        return value.startsWith("https://") || value.startsWith("http://");
+    }
+}
+
