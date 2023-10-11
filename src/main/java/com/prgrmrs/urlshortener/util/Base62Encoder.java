1ffc49dd2d0b7e77055c66fb7675033096cd50f1
Jaeyoung Ahn <quaki@naver.com>
2023-10-11 16:52:21 +0900

    refactor: base62 로직 변경

--- src/main/java/com/prgrmrs/urlshortener/util/Base62Encoder.java
+++ src/main/java/com/prgrmrs/urlshortener/util/Base62Encoder.java
@@ -1,14 +1,18 @@
 package com.prgrmrs.urlshortener.util;
 
-public class Base62 {
+public class Base62Encoder {
 
     static final char[] BASE62 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+
+    private Base62Encoder() {
+        // on purpose
+    }
 
     public static String encode(Long sequence) {
         final StringBuilder sb = new StringBuilder();
         do {
             int i = (int) (sequence % 62);
-            sb.append(BASE62[i]);
+            sb.insert(0, BASE62[i]);  // Prepend the character
             sequence /= 62;
         } while (sequence > 0);
         return sb.toString();
