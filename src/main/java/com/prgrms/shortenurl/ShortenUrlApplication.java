f21a7ccea5a4602083845398a0c98124b04203c0
eugene <81062608+eugene225@users.noreply.github.com>
2023-12-07 14:34:07 +0900

    init commit

---
+++ src/main/java/com/prgrms/shortenurl/ShortenUrlApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.shortenurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortenUrlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prgrms/shortenurl/ShortenUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.shortenurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
