ec5e7ca867a809a0d59bdee726603b898d495720
EunChanNam <namhm23@gmail.com>
2023-12-10 00:51:40 +0900

    feat: UrlRepository 및 UrlJpaRepository 구현

---
+++ src/main/java/com/dev/shortenerurl/url/domain/UrlRepository.java
@@ -0,0 +1,4 @@
+package com.dev.shortenerurl.url.domain;
+
+public interface UrlRepository {
+}
---
+++ src/main/java/com/dev/shortenerurl/url/infrastructure/UrlJpaRepository.java
@@ -0,0 +1,9 @@
+package com.dev.shortenerurl.url.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.shortenerurl.url.domain.UrlRepository;
+import com.dev.shortenerurl.url.domain.model.Url;
+
+public interface UrlJpaRepository extends JpaRepository<Url, Long>, UrlRepository {
+}
