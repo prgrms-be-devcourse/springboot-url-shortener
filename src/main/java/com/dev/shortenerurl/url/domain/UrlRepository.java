f8610c8557f9a887128a976fac1e8e3fa058ffcb
EunChanNam <namhm23@gmail.com>
2023-12-10 01:21:46 +0900

    feat: UrlRepository - save, findById, findByOriginUrl 구현

--- src/main/java/com/dev/shortenerurl/url/domain/UrlRepository.java
+++ src/main/java/com/dev/shortenerurl/url/domain/UrlRepository.java
@@ -1,4 +1,14 @@
 package com.dev.shortenerurl.url.domain;
 
+import java.util.Optional;
+
+import com.dev.shortenerurl.url.domain.model.Url;
+
 public interface UrlRepository {
+
+	Url save(Url url);
+
+	Optional<Url> findById(Long id);
+
+	Optional<Url> findByOriginUrl(String originUrl);
 }
--- src/main/java/com/dev/shortenerurl/url/infrastructure/UrlJpaRepository.java
+++ src/main/java/com/dev/shortenerurl/url/infrastructure/UrlJpaRepository.java
@@ -1,9 +1,13 @@
 package com.dev.shortenerurl.url.infrastructure;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dev.shortenerurl.url.domain.UrlRepository;
 import com.dev.shortenerurl.url.domain.model.Url;
 
 public interface UrlJpaRepository extends JpaRepository<Url, Long>, UrlRepository {
+
+	Optional<Url> findByOriginUrl(String originUrl);
 }
