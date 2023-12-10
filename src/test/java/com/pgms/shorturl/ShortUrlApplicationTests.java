bbbfbe0b9f70856c6affeecef8fea5aa47fc51e7
KarmaPol <kkhkr98@gmail.com>
2023-12-10 19:03:07 +0900

    feat : first commit

---
+++ src/test/java/com/pgms/shorturl/ShortUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgms.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
