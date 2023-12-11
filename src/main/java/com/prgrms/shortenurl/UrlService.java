74b0d627583dcc88812fce4b0ed8a7dd61d716ce
eugene <81062608+eugene225@users.noreply.github.com>
2023-12-11 13:07:36 +0900

    refactor: 기본 service 구성

---
+++ src/main/java/com/prgrms/shortenurl/UrlService.java
@@ -0,0 +1,46 @@
+package com.prgrms.shortenurl;
+
+import com.google.common.hash.Hashing;
+import com.prgrms.shortenurl.domain.Url;
+import com.prgrms.shortenurl.domain.UrlRepository;
+import lombok.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.charset.Charset;
+
+@Service
+public class UrlService {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final UrlRepository urlRepository;
+    public UrlService(UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
+
+    private String shortenUrlByMurMur(@NonNull String originUrl) {
+        return Hashing.murmur3_32_fixed().hashString(originUrl, Charset.defaultCharset()).toString();
+    }
+
+    @Transactional
+    public Url addLink(@NonNull String originUrl) {
+        String key = shortenUrlByMurMur(originUrl);
+        Url url = urlRepository.findByShortenKey(key)
+                .orElse(
+                        Url.builder()
+                                .shortenKey(key)
+                                .originUrl(originUrl)
+                                .shortenUrl("http://localhost:8090/" + key).build()
+                );
+        return urlRepository.save(url);
+    }
+
+    @Transactional(readOnly = true)
+    public String getUrlByKey(@NonNull String key) {
+        Url url = urlRepository.findByShortenKey(key)
+                .orElseThrow(() -> new IllegalArgumentException("해당 url을 찾을 수 없습니다."));
+        return url.getOriginUrl();
+    }
+}
