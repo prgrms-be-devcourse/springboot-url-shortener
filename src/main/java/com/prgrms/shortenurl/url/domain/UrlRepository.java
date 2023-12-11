43a530d036290d410538702f02582c27e5ecea9b
eugene <81062608+eugene225@users.noreply.github.com>
2023-12-11 13:04:17 +0900

    Chore: 패키지 수정

--- src/main/java/com/prgrms/shortenurl/url/domain/UrlRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prgrms.shortenurl.url.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UrlRepository extends JpaRepository<Url, String> {
-    Optional<Url> findByShortenKey(String key);
-}
