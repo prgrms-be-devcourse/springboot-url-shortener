a8d1eda476ce977a48b2b8a655785012e81e5657
KarmaPol <kkhkr98@gmail.com>
2023-12-11 22:48:15 +0900

    feat : add 레디스 캐시

--- short-url-api/src/main/java/com/pgms/shorturlapi/url/UrlService.java
+++ short-url-api/src/main/java/com/pgms/shorturlapi/url/UrlService.java
@@ -3,6 +3,7 @@ package com.pgms.shorturlapi.url;
 import com.pgms.shorturlcoredomain.url.Url;
 import com.pgms.shorturlcoredomain.url.UrlRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.pgms.shorturlcoredomain.util.Base62Converter;
@@ -25,6 +26,7 @@ public class UrlService {
         return findUrl.getShortUrl();
     }
 
+    @Cacheable(value = "originalUrl", key = "#shortUrl", cacheManager = "contentCacheManager")
     public String getOriginalUrl(String shortUrl){
         return urlRepository.findUrlByShortUrl(shortUrl).orElseThrow(RuntimeException::new).getOriginal();
     }
--- short-url-core/short-url-core-infra/src/main/java/com/pgms/shorturlcoreinfra/config/RedisConfig.java
+++ short-url-core/short-url-core-infra/src/main/java/com/pgms/shorturlcoreinfra/config/RedisConfig.java
@@ -13,10 +13,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String host;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
     @Bean
