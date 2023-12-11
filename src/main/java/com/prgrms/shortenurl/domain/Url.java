3e78f0592a8bdccb51ec1d7d214cff0cb9359b76
eugene <81062608+eugene225@users.noreply.github.com>
2023-12-11 13:04:57 +0900

    chore: 패키지 구조 변경

---
+++ src/main/java/com/prgrms/shortenurl/domain/Url.java
@@ -0,0 +1,34 @@
+package com.prgrms.shortenurl.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "shortenUrl")
+    @SequenceGenerator(name = "shortenUrl", sequenceName = "shortenUrl", allocationSize = 1)
+    private Long id;
+
+    @NonNull
+    @Column(name = "shorten_key")
+    private String shortenKey;
+
+    @NonNull
+    @Column(name = "orgin_url")
+    private String originUrl;
+
+    @NonNull
+    @Column(name = "shorten_url")
+    private String shortenUrl;
+
+    @Builder
+    public Url(@NonNull String shortenKey, @NonNull String originUrl, @NonNull String shortenUrl) {
+        this.shortenKey = shortenKey;
+        this.originUrl = originUrl;
+        this.shortenUrl = shortenUrl;
+    }
+}
---
+++ src/main/java/com/prgrms/shortenurl/domain/UrlRepository.java
@@ -0,0 +1,9 @@
+package com.prgrms.shortenurl.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<Url, String> {
+    Optional<Url> findByShortenKey(String key);
+}
