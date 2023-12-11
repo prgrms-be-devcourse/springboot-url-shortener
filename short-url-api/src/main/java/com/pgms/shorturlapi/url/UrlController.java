0fad00c6566dcf8ed0c5d030ed74f1952eb7d75f
KarmaPol <kkhkr98@gmail.com>
2023-12-11 18:23:26 +0900

    feat : url controller 완

--- short-url-api/src/main/java/com/pgms/shorturlapi/url/UrlController.java
+++ short-url-api/src/main/java/com/pgms/shorturlapi/url/UrlController.java
@@ -1,13 +1,11 @@
 package com.pgms.shorturlapi.url;
 
 import com.pgms.shorturlcoredomain.common.response.CommonResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -18,14 +16,17 @@ public class UrlController {
     private final UrlService urlService;
 
     @PostMapping("/short-url")
-    public CommonResponse<String> getShortUrl(@RequestParam String url){
+    public CommonResponse<String> getShortUrl(@RequestParam String url, HttpServletRequest request){
         String shortUrl = urlService.getShortUrl(url);
 
-        return new CommonResponse<>(HttpStatus.OK.value(), shortUrl);
+        StringBuffer sb = new StringBuffer(request.getRequestURL().toString().replace(request.getRequestURI(), ""));
+        sb.append("/").append(shortUrl);
+
+        return new CommonResponse<>(HttpStatus.OK.value(), sb.toString());
     }
 
-    @GetMapping("/")
-    public void redirectUrl(@RequestParam String shortUrl, HttpServletResponse response) throws IOException {
+    @GetMapping("/{shortUrl}")
+    public void redirectUrl(@PathVariable String shortUrl, HttpServletResponse response) throws IOException {
         String originalUrl = urlService.getOriginalUrl(shortUrl);
         response.sendRedirect(originalUrl);
     }
