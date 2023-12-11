95b208bb3dcb7e03aaf596f9add65f17f9978e2c
dlswns2480 <dlswns2480@naver.com>
2023-12-11 23:24:59 +0900

    feat : 컨트롤러 타임리프 관련 에러 수정 및 최종 구현

--- src/main/java/com/devcourse/shorturl/url/presentation/UrlController.java
+++ src/main/java/com/devcourse/shorturl/url/presentation/UrlController.java
@@ -2,7 +2,6 @@ package com.devcourse.shorturl.url.presentation;
 
 import com.devcourse.shorturl.url.application.UrlService;
 import com.devcourse.shorturl.url.dto.CreateUrlResponse;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,30 +9,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
 public class UrlController {
     private final UrlService urlService;
 
-    @GetMapping("/")
-    public String showForm(){
+    @GetMapping
+    public String showForm() {
         return "shortenUrlForm";
     }
 
     @PostMapping("/shorten")
-    public String shortenUrl(@RequestParam("originalUrl") String originalUrl, Model model) {
+    public String shortenUrl(@RequestParam("originalUrl") String originalUrl, RedirectAttributes redirectAttributes) {
         CreateUrlResponse response = urlService.createShortUrl(originalUrl);
+        redirectAttributes.addAttribute("originalUrl", originalUrl);
+        redirectAttributes.addAttribute("shortenedUrl", response.shortUrl());
+        redirectAttributes.addAttribute("hits", response.hits());
 
+        return "redirect:/shortened";
+    }
+    @GetMapping("/shortened")
+    public String shortenedUrlPage(@RequestParam("originalUrl") String originalUrl,
+                             @RequestParam("shortenedUrl") String shortenedUrl,
+                             @RequestParam("hits") String hits,
+                             Model model){
         model.addAttribute("originalUrl", originalUrl);
-        model.addAttribute("shortenedUrl", response.shortUrl());
-        model.addAttribute("hits", response.hits());
-
+        model.addAttribute("shortenedUrl", shortenedUrl);
+        model.addAttribute("hits", hits);
         return "shortenedUrlResult";
     }
 
     @GetMapping("/{shortUrl}")
-    public String redirectLongUrl(@PathVariable("shortUrl") String shortUrl){
+    public String redirectLongUrl(@PathVariable String shortUrl) {
         String longUrl = urlService.redirectLongUrl(shortUrl);
         return "redirect:" + longUrl;
     }
