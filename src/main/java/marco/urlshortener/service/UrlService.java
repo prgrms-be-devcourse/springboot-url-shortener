d2de4aa6fcb2e6bf5d420978735a55984ca3461d
Jay <injaesong0@gmail.com>
2023-12-12 12:13:00 +0900

    feat: short URL 생성 추가

--- src/main/java/marco/urlshortener/service/UrlService.java
+++ src/main/java/marco/urlshortener/service/UrlService.java
@@ -1,22 +1,30 @@
 package marco.urlshortener.service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import marco.urlshortener.domain.Url;
 import marco.urlshortener.repositoy.UrlRepository;
+import marco.urlshortener.util.Base62Encoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UrlService {
     private final UrlRepository urlRepository;
 
+    @Transactional
     public String getShortUrl(String longUrl) {
         Optional<Url> url = urlRepository.findByLongUrl(longUrl);
 
         if (url.isEmpty()) {
-            // TODO: short url 만들기
+            Url savedUrl = urlRepository.save(new Url(longUrl));
+            Long id = savedUrl.getId();
+            String encodedUrl = Base62Encoder.toBase62(id);
+
+            savedUrl.setShortUrl(encodedUrl);
+
+            return savedUrl.getShortUrl();
         }
 
         return url.get()
