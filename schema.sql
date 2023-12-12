40742b13966e7b2703059c31c8c8f3663c5cb64a
EomYerim <djadpfa1002@naver.com>
2023-12-12 11:24:22 +0900

    chore: schema url 필드명 변경

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-CREATE TABLE URL(
+create TABLE URL(
     id BIGINT AUTO_INCREMENT,
-    longUrl VARCHAR(255) NOT NULL UNIQUE,
-    shortUrl CHAR(8) UNIQUE,
+    long_url VARCHAR(255) NOT NULL UNIQUE,
+    short_url CHAR(8) UNIQUE,
     CONSTRAINT pk_url_id PRIMARY KEY (id)
 );
