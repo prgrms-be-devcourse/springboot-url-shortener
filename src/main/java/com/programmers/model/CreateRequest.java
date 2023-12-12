33e894f998bb16b2a0da09f346edc526fb163442
voidmelody <sss9073@naver.com>
2023-12-12 23:22:50 +0900

    feat: "thymeleaf 객체 바인딩을 위한 setter 추가"

--- src/main/java/com/programmers/model/CreateRequest.java
+++ src/main/java/com/programmers/model/CreateRequest.java
@@ -2,9 +2,11 @@ package com.programmers.model;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.URL;
 
 @Getter
+@Setter
 public class CreateRequest {
     @NotBlank(message = "URL 값이 필요합니다.")
     @URL(message = "URL 형태가 아닙니다.")
