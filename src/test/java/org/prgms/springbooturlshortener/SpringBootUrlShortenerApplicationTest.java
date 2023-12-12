8a638f93a285ea57606ec288177428b0802a9735
ZZAMBAs <rksidksrksi@naver.com>
2023-12-12 10:03:20 +0900

    Initial commit

---
+++ src/test/java/org/prgms/springbooturlshortener/SpringBootUrlShortenerApplicationTest.java
@@ -0,0 +1,12 @@
+package org.prgms.springbooturlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUrlShortenerApplicationTest {
+    @Test
+    void contextLoad() {
+
+    }
+}
