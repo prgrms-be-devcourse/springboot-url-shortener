75140c2433c52d4343cae6e685817f13090e2b05
ASak1104 <khw56184@gmail.com>
2023-12-13 11:56:17 +0900

    feat: decode 기능 추가

--- src/main/java/marco/urlshortener/util/Base62.java
+++ src/main/java/marco/urlshortener/util/Base62.java
@@ -1,19 +1,32 @@
 package marco.urlshortener.util;
 
-public class Base62Encoder {
+public final class Base62 {
+
     private static final String BASE62_CHAR = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final int BASE = 62;
 
-    private Base62Encoder() {
+    private Base62() {
     }
 
-    public static String toBase62(long longUrlId) {
+    public static String encode(long longUrlId) {
         String result = "";
 
         while (longUrlId > 0) {
             int remind = (int) (longUrlId % BASE);
             longUrlId /= BASE;
             result += BASE62_CHAR.charAt(remind);
+        }
+
+        return result;
+    }
+
+    public static long decode(String shortUrl) {
+        long result = 0L;
+        long power = 1L;
+
+        for (int i = 0; i < shortUrl.length(); i++) {
+            result += power * BASE62_CHAR.indexOf(shortUrl.charAt(i));
+            power *= BASE;
         }
 
         return result;
