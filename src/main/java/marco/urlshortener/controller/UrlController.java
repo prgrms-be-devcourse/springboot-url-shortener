fed704ab045693bc0fc4da5fc65610f65fcb9745
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-13 13:49:25 +0900

    feat: url 리다이렉트 요청 기능 구현

--- src/main/java/marco/urlshortener/controller/UrlController.java
+++ src/main/java/marco/urlshortener/controller/UrlController.java
@@ -3,10 +3,10 @@ package marco.urlshortener.controller;
 import lombok.RequiredArgsConstructor;
 import marco.urlshortener.dto.UrlRequest;
 import marco.urlshortener.service.UrlService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,7 +14,15 @@ public class UrlController {
     private final UrlService urlService;
 
     @PostMapping("/url")
-    public ResponseEntity<String> makeUrl(@RequestBody UrlRequest request){
+    public ResponseEntity<String> makeUrl(@RequestBody UrlRequest request) {
         return ResponseEntity.ok(urlService.getShortUrl(request.longUrl()));
+    }
+
+    @GetMapping("/url/{shortUrl}")
+    public ResponseEntity<String> redirectUrl(@PathVariable String shortUrl) {
+        String longUrl = urlService.getLongUrl(shortUrl);
+        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
+                .header(HttpHeaders.LOCATION, longUrl)
+                .build();
     }
 }
