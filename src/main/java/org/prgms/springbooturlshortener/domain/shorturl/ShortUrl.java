9e9b702831f1b21e13d8fe056703bb7aed43e7ad
ZZAMBAs <rksidksrksi@naver.com>
2023-12-13 23:33:42 +0900

    Feat: ShortUrl에 빌더 패턴을 추가한다.

--- src/main/java/org/prgms/springbooturlshortener/domain/shorturl/ShortUrl.java
+++ src/main/java/org/prgms/springbooturlshortener/domain/shorturl/ShortUrl.java
@@ -2,6 +2,8 @@ package org.prgms.springbooturlshortener.domain.shorturl;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
@@ -10,6 +12,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ShortUrl {
     @Id
+    @Getter
     private String transformedUrl;
 
     private String originalUrl;
@@ -17,4 +20,11 @@ public class ShortUrl {
     private Long visitCount;
 
     private LocalDateTime lastVisitTime;
+
+    @Builder
+    public ShortUrl(String transformedUrl, String originalUrl) {
+        this.transformedUrl = transformedUrl;
+        this.originalUrl = originalUrl;
+        this.visitCount = 0L;
+    }
 }
