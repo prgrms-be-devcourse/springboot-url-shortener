d9f88c4fb3e8e73c47088936ab41a7462269e5c6
ebcho <joeunbi514@naver.com>
2023-12-13 17:08:46 +0900

    프로젝트 세팅

---
+++ src/main/java/com/dev/urlshortner/UrlShortnerApplication.java
@@ -0,0 +1,13 @@
+package com.dev.urlshortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerApplication.class, args);
+	}
+
+}
