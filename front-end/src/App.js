2cefea1b9f2514f1e0e40fcc76c129dd48351066
shoeone96 <dlwnddnjs96@gmail.com>
2023-12-15 00:06:14 +0900

    feat: main 페이지 form 전송 화면 구현

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,73 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { Button, Form, Input, message, Space } from 'antd';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [form] = Form.useForm();
+  const onFinish = () => {
+    message.success('Submit success!');
+  };
+  const submit = () => {
+    axios.post('http://localhost:8080/url', {longUrl:'https://www.naver.com'})
+    .then(
+      data => {
+        console.log(data);
+      }
+    ).catch(
+      (e) => {
+          console.log(e);
+      }
+    );
+  }
+  const onFinishFailed = () => {
+    message.error('Submit failed!');
+  };
+  const onFill = () => {
+    form.setFieldsValue({
+      url: 'https://taobao.com/',
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form
+      form={form}
+      layout="vertical"
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        name="url"
+        label="URL"
+        rules={[
+          {
+            required: true,
+          },
+          {
+            type: 'url',
+            warningOnly: true,
+          },
+          {
+            type: 'string',
+            min: 6,
+          },
+        ]}
+      >
+        <Input placeholder="input placeholder" />
+      </Form.Item>
+      <Form.Item>
+        <Space>
+          <Button type="primary" htmlType="submit" onClick={submit}>
+            제출
+          </Button>
+          <Button htmlType="button" onClick={onFill}>
+            Fill
+          </Button>
+        </Space>
+      </Form.Item>
+    </Form>
   );
-}
+};
 
 export default App;
