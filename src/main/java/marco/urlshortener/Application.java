d230f422fa8556d0b974ef906b130d2006771a6f
shoeone96 <dlwnddnjs96@gmail.com>
2023-12-15 00:04:36 +0900

    feat: CORS 설정 추가

--- src/main/java/marco/urlshortener/Application.java
+++ src/main/java/marco/urlshortener/Application.java
@@ -2,6 +2,9 @@ package marco.urlshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,13 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
