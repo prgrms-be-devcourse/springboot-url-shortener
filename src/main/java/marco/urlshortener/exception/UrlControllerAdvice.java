781597d88cac010ef7fe826921597e722beea1fd
shoeone96 <dlwnddnjs96@gmail.com>
2023-12-14 13:36:33 +0900

    feat: MethodArgumentNotValidException 예외 처리 validation 이 실패한 경우의 예외처리

--- src/main/java/marco/urlshortener/exception/UrlControllerAdvice.java
+++ src/main/java/marco/urlshortener/exception/UrlControllerAdvice.java
@@ -2,14 +2,21 @@ package marco.urlshortener.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 @Slf4j
 @RestControllerAdvice
 public class UrlControllerAdvice {
+    private static final String DEFAULT_MESSAGE = "유효하지 않은 요청입니다.";
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
         log.error("Unexpected error occurred with the given message : {}", e.getMessage(), e);
@@ -24,4 +31,20 @@ public class UrlControllerAdvice {
         return ResponseEntity.notFound()
                 .build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<FieldError> errors = e.getFieldErrors();
+        String message = errors.stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(DEFAULT_MESSAGE);
+
+        ErrorResponse errorResponse = new ErrorResponse(message);
+
+        return ResponseEntity.badRequest()
+                .body(errorResponse);
+    }
+
 }
