e3265b0e20429fed0f60e64a81e063c7b3d8ccfd
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-14 15:04:42 +0900

    test: getShortUrl 메서드 테스트 추가

---
+++ src/test/java/marco/urlshortener/service/UrlServiceTest.java
@@ -0,0 +1,63 @@
+package marco.urlshortener.service;
+
+import marco.urlshortener.domain.Url;
+import marco.urlshortener.dto.UrlRequest;
+import marco.urlshortener.dto.UrlResponse;
+import marco.urlshortener.repositoy.UrlRepository;
+import marco.urlshortener.util.Base62;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class UrlServiceTest {
+    @Autowired
+    UrlService urlService;
+    @Autowired
+    UrlRepository urlRepository;
+
+    @BeforeEach
+    void setUp() {
+        urlRepository.deleteAll();
+    }
+
+    @DisplayName("이미 존재하는 단축된 url을 조회한다.")
+    @Test
+    void testGetExistingShortUrl() {
+        // given
+        String longUrl = "https://www.naver.com";
+        UrlRequest urlRequest = new UrlRequest(longUrl);
+        Url expectedUrl = urlRepository.save(new Url(longUrl));
+        String shortUrlKey = Base62.encode(expectedUrl.getId());
+
+        // when
+        UrlResponse actualUrl = urlService.getShortUrl(urlRequest);
+
+        // then
+        assertThat(actualUrl).isNotNull();
+        assertThat(actualUrl.url()).endsWith(shortUrlKey);
+    }
+
+    @DisplayName("새로운 단축된 url을 생성한 후 반횐한다.")
+    @Test
+    void testGetNewShortUrl() {
+        // given
+        String longUrl = "https://www.naver.com";
+        UrlRequest urlRequest = new UrlRequest(longUrl);
+
+        // when
+        UrlResponse actualUrl = urlService.getShortUrl(urlRequest);
+
+        // then
+        Optional<Url> expectedUrl = urlRepository.findByLongUrl(longUrl);
+        String shortUrlKey = Base62.encode(expectedUrl.get().getId());
+        assertThat(actualUrl).isNotNull();
+        assertThat(actualUrl.url()).endsWith(shortUrlKey);
+    }
+}
