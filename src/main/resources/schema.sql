960b602313681dd9c07bee42f116be290092bd7c
ZZAMBAs <rksidksrksi@naver.com>
2023-12-15 00:39:34 +0900

    Fix: 일부 잘못된 환경 설정, visitCount를 더하는 쿼리를 제대로 수정한다.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS short_url (
     transformed_url char(6) PRIMARY KEY,
     original_url varchar(2047) not null,
     visit_count bigint default 0 not null,
-    last_visit_time timestamp not null default current_timestamp(6)
+    last_visit_time timestamp(6) not null default current_timestamp(6)
 );
