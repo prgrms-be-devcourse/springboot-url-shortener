f57f6c3383d8b198fc3f93defa751a85726a732a
marooo326 <ktnqkr01@gmail.com>
2023-12-14 21:22:18 +0900

    fix: URL 유효성 검증 로직 수정

--- src/main/java/org/daehwi/shorturl/util/UrlValidator.java
+++ src/main/java/org/daehwi/shorturl/util/UrlValidator.java
@@ -1,25 +1,38 @@
 package org.daehwi.shorturl.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.daehwi.shorturl.controller.dto.ResponseStatus;
+import org.daehwi.shorturl.exception.CustomException;
 
 import java.net.URL;
 
 @Slf4j
 public class UrlValidator {
+
+    private static final String HTTP_SCHEME = "http://";
+    private static final String HTTPS_SCHEME = "https://";
+
     public static String validateUrl(String urlString) {
         try {
+            if (!isProtocolExist(urlString)) {
+                urlString = HTTP_SCHEME + urlString;
+            }
             new URL(urlString);
             return getCleanURL(urlString);
         } catch (Exception e) {
-            log.error("URL validation failed. Url :{}", urlString);
-            throw new IllegalArgumentException("Invalid URL");
+            e.printStackTrace();
+            throw new CustomException(ResponseStatus.INVALID_URL);
         }
+    }
+
+    private static boolean isProtocolExist(String urlString) {
+        return urlString.startsWith(HTTP_SCHEME) || urlString.startsWith(HTTPS_SCHEME);
     }
 
     private static String getCleanURL(String urlString) {
         urlString = urlString
-                .replace("http://", "")
-                .replace("https://", "");
+                .replace(HTTP_SCHEME, "")
+                .replace(HTTPS_SCHEME, "");
         return urlString;
     }
 }
