fb10ad88e30fc8c6fdcc2aa546a377e8d3b0cbc0
marooo326 <ktnqkr01@gmail.com>
2023-12-15 21:44:10 +0900

    feat: 비관적 락 적용

--- src/main/java/org/daehwi/shorturl/repository/UrlRepository.java
+++ src/main/java/org/daehwi/shorturl/repository/UrlRepository.java
@@ -1,10 +1,13 @@
 package org.daehwi.shorturl.repository;
 
+import jakarta.persistence.LockModeType;
 import org.daehwi.shorturl.domain.ShortUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface UrlRepository extends JpaRepository<ShortUrl, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<ShortUrl> findByShortUrl(String shortUrl);
 }
