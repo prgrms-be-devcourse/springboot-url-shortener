fb68d3c73ca1bef790e52a904fc1d026c22d61eb
shoeone96 <dlwnddnjs96@gmail.com>
2023-12-15 16:23:15 +0900

    style: 소스코드 formatting

--- src/main/java/marco/urlshortener/Application.java
+++ src/main/java/marco/urlshortener/Application.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
