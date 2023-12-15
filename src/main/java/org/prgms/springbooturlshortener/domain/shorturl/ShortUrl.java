7d638e72f4de906f864b0fa743b3de21bc43aaff
ZZAMBAs <rksidksrksi@naver.com>
2023-12-15 23:03:24 +0900

    Feat: null은 쿼리에 포함시키지 않도록 하고 originalUrl에 getter를 추가한다.

--- src/main/java/org/prgms/springbooturlshortener/domain/shorturl/ShortUrl.java
+++ src/main/java/org/prgms/springbooturlshortener/domain/shorturl/ShortUrl.java
@@ -6,21 +6,26 @@ import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
+@DynamicInsert
+@DynamicUpdate
 public class ShortUrl {
     @Id
     @Getter
     @Column(name = "transformed_url")
     private String transformedUrl;
 
-    @Column(name = "original_url")
+    @Getter
+    @Column(name = "original_url", nullable = false)
     private String originalUrl;
 
-    @Column(name = "visit_count")
+    @Column(name = "visit_count", nullable = false)
     private Long visitCount;
 
     @Column(name = "last_visit_time")
