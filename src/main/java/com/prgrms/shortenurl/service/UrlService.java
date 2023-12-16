5165c187d45f37bc1759a39ec5bb5a6f77de7428
eugene <81062608+eugene225@users.noreply.github.com>
2023-12-17 02:32:59 +0900

    refactor: 인코딩 함수 로직 변경

--- src/main/java/com/prgrms/shortenurl/service/UrlService.java
+++ src/main/java/com/prgrms/shortenurl/service/UrlService.java
@@ -5,7 +5,6 @@ import com.prgrms.shortenurl.domain.Url;
 import com.prgrms.shortenurl.domain.UrlRepository;
 import com.prgrms.shortenurl.exception.UrlNotFoundException;
 import lombok.NonNull;
-import org.h2.value.ValueVarbinary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -19,24 +18,9 @@ public class UrlService {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final UrlRepository urlRepository;
+
     public UrlService(UrlRepository urlRepository) {
         this.urlRepository = urlRepository;
-    }
-
-    private String shortenUrlByMurMur(@NonNull String originUrl) {
-        return Hashing.murmur3_32_fixed().hashString(originUrl, Charset.defaultCharset()).toString();
-    }
-
-    private String shortenUrlByBase62(@NonNull Long id) {
-        int intKey = Math.toIntExact(id);
-
-        // 이진수 변환
-        String binary = Converter.convertToBinary(intKey);
-
-        // 16진수 변환
-        String hex = Converter.convertToHex(Integer.parseInt(binary));
-
-        return Base62.encode(HexFormat.fromHexDigitsToLong(hex));
     }
 
     private Url save(@NonNull String originUrl) {
@@ -44,10 +28,26 @@ public class UrlService {
                 .originUrl(originUrl).build());
     }
 
+    public static String shortenUrlByMurMur(@NonNull Url url) {
+        return Hashing.murmur3_32_fixed().hashString(url.getOriginUrl(), Charset.defaultCharset()).toString();
+    }
+
+    public static String shortenUrlByBase62(@NonNull Url url) {
+        int intKey = Math.toIntExact(url.getId());
+
+        // 이진수 변환
+        String binary = Converter.convertToBinary(intKey);
+
+        // 16진수 변환
+        String hex = Converter.convertToHex(Integer.parseInt(binary));
+
+        return Base62Encoding.encode(HexFormat.fromHexDigitsToLong(hex));
+    }
+
     @Transactional
-    public Url addLink(@NonNull String originUrl) {
+    public Url addLink(@NonNull String originUrl, @NonNull String encodingType) {
         Url url = save(originUrl);
-        String key = shortenUrlByBase62(url.getId());
+        String key = EnumConverter.convert(encodingType).encode(url);
         url.updateShortenUrl(key);
         return url;
     }
