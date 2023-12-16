cae06c172e751966e16bc30d5e864638259902ac
ebcho <joeunbi514@naver.com>
2023-12-16 19:18:18 +0900

    feat: 레포지토리 생성

---
+++ src/main/java/com/dev/urlshortner/repository/UrlRepository.java
@@ -0,0 +1,13 @@
+package com.dev.urlshortner.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.urlshortner.entity.Url;
+
+public interface UrlRepository extends JpaRepository<Url, Long> {
+	Optional<Url> findByShortKey(String shortKey);
+
+	boolean existsByShortKey(String shortKey);
+}
