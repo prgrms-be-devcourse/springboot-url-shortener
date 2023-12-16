92f075898efce756ed64763326de27c305fcefc5
ebcho <joeunbi514@naver.com>
2023-12-16 19:21:53 +0900

    feat: 알고리즘 유형 enum 생성

---
+++ src/main/java/com/dev/urlshortner/global/EncodingType.java
@@ -0,0 +1,20 @@
+package com.dev.urlshortner.global;
+
+import java.util.function.Supplier;
+
+import com.dev.urlshortner.util.Base62RandomKeyGenerator;
+import com.dev.urlshortner.util.ShortKeyGenerator;
+
+public enum EncodingType {
+	BASE62(Base62RandomKeyGenerator::new);
+
+	private final Supplier<ShortKeyGenerator> encoderSupplier;
+
+	EncodingType(Supplier<ShortKeyGenerator> encoderSupplier) {
+		this.encoderSupplier = encoderSupplier;
+	}
+
+	public ShortKeyGenerator getEncoder() {
+		return encoderSupplier.get();
+	}
+}
