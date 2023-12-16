2f3545443a195da18a661d3603ce5934fb4851aa
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-12-17 01:43:36 +0900

    feat: 존재하지 않는 단축 url 예외 처리 추가

--- src/main/java/com/springboot/springbooturlshortner/service/UrlService.java
+++ src/main/java/com/springboot/springbooturlshortner/service/UrlService.java
@@ -1,15 +1,19 @@
 package com.springboot.springbooturlshortner.service;
 
 import com.springboot.springbooturlshortner.domain.Url;
+import com.springboot.springbooturlshortner.exception.UrlException;
+import com.springboot.springbooturlshortner.exception.UrlExceptionCode;
 import com.springboot.springbooturlshortner.repository.UrlRepository;
 import com.springboot.springbooturlshortner.util.Base62Util;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UrlService {
 
     private final UrlRepository urlRepository;
@@ -27,7 +31,7 @@ public class UrlService {
 
     @Transactional
     public String getOriginUrl(String shortenUrl) {
-        Url url = urlRepository.findById(base62Util.decoding(shortenUrl)).get();
+        Url url = urlRepository.findById(base62Util.decoding(shortenUrl)).orElseThrow(() -> new UrlException(UrlExceptionCode.NOT_FOUND));
         url.increaseRequestCntOne();
 
         return url.getOriginUrl();
