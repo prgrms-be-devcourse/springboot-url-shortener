d10591189f06d7e1c521d9b3d9b20fa960b9b223
hs12 <loveanna1228@gmail.com>
2023-12-17 17:44:13 +0900

    [Feat] Shortening key 생성 알고리즘 추가

---
+++ urlshortener/src/main/java/com/prgrms/url_shortener/algorithm/Base62Algorithm.java
@@ -0,0 +1,32 @@
+package com.prgrms.url_shortener.algorithm;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Base62Algorithm {
+
+    private static final char[] BASE62_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+    private static final int BASE62_LENGTH = 62;
+
+    public static String encode(Long value) {
+        final StringBuilder sb = new StringBuilder();
+        while (value > 0) {
+            sb.append(BASE62_CHARS[(int) (value % BASE62_LENGTH)]);
+            value /= BASE62_LENGTH;
+        }
+        return sb.toString();
+    }
+
+    public static Long decode(String value) {
+        long result = 0;
+        long power = 1;
+        for (int i = 0; i < value.length(); i++) {
+            result += new String(BASE62_CHARS).indexOf(value.charAt(i)) * power;
+            power *= BASE62_LENGTH;
+        }
+        return result;
+    }
+}
