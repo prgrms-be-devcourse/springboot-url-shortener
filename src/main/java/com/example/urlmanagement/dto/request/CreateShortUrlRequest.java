9b2a411892b04eab5ac7dac852acff34991eb236
letskuku <yujung9288@naver.com>
2023-12-18 01:28:30 +0900

    feat: dto에 builder 추가

--- src/main/java/com/example/urlmanagement/dto/request/CreateShortUrlRequest.java
+++ src/main/java/com/example/urlmanagement/dto/request/CreateShortUrlRequest.java
@@ -1,13 +1,20 @@
 package com.example.urlmanagement.dto.request;
 
 import com.example.urlmanagement.domain.Url;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class CreateShortUrlRequest {
 
-    private String originalUrl;
-    private String encodeType;
+    private final String originalUrl;
+    private final String encodeType;
+
+    @Builder
+    public CreateShortUrlRequest(String originalUrl, String encodeType) {
+        this.originalUrl = originalUrl;
+        this.encodeType = encodeType;
+    }
 
     public Url toUrl() {
         return Url.builder()
