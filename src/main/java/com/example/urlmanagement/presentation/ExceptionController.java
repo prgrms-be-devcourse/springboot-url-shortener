a118f9e54ef75327e1e1b97fa45095599a2876c6
letskuku <yujung9288@naver.com>
2023-12-18 01:51:42 +0900

    chore: ExceptionHandler 어노테이션 추가

--- src/main/java/com/example/urlmanagement/presentation/ExceptionController.java
+++ src/main/java/com/example/urlmanagement/presentation/ExceptionController.java
@@ -5,22 +5,26 @@ import com.example.urlmanagement.exception.UrlNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionController {
 
+    @ExceptionHandler(EncodeTypeNotFoundException.class)
     public ResponseEntity<String> catchEncodeTypeNotFoundException(EncodeTypeNotFoundException e) {
         log.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    @ExceptionHandler(UrlNotFoundException.class)
     public ResponseEntity<String> catchUrlNotFoundException(UrlNotFoundException e) {
         log.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<String> catchGlobalException(Exception e) {
         log.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("예상치 못한 예외가 발생했습니다.");
