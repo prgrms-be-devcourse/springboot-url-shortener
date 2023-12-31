75ab3712d55f690b62120bd9b8f759ce15097e66
KarmaPol <kkhkr98@gmail.com>
2023-12-31 19:02:59 +0900

    fix : 패키지 정리

--- short-url-core/short-url-core-infra/src/test/java/com/pgms/shorturlcoreinfra/ShortUrlCoreInfraApplicationTests.java
+++ short-url-core/short-url-core-infra/src/test/java/com/pgms/shorturlcoreinfra/ShortUrlCoreInfraApplicationTests.java
@@ -1,9 +1,7 @@
 package com.pgms.shorturlcoreinfra;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ShortUrlCoreInfraApplicationTests {
 
     @Test
