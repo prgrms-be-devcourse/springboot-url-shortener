3a248f5800bd05fba2347f18768e3e874d216dcd
ZZAMBAs <rksidksrksi@naver.com>
2024-01-02 00:53:11 +0900

    Chore: 상수 네이밍을 통일한다.

--- src/main/java/org/prgms/springbooturlshortener/domain/shorturl/service/transformer/Base62UrlTransformer.java
+++ src/main/java/org/prgms/springbooturlshortener/domain/shorturl/service/transformer/Base62UrlTransformer.java
@@ -7,13 +7,13 @@ import org.springframework.stereotype.Component;
 @Qualifier("BASE62")
 public class Base62UrlTransformer implements UrlTransformer{
     private static final String BASE62_TOKENS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    private static final short numOfUrl = 6;
+    private static final short NUM_OF_URL = 6;
     private static final int DIVIDE_NUM = 62;
 
     public String generateUrl(int number) {
         StringBuilder sb = new StringBuilder();
 
-        for (short i = 0; i < numOfUrl; i++) {
+        for (short i = 0; i < NUM_OF_URL; i++) {
             char token = BASE62_TOKENS.charAt(number % DIVIDE_NUM);
 
             sb.append(token);
