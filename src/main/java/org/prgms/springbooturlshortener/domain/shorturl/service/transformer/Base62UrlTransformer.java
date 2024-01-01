9ea0a29d255172454bc008224eac3b3f3c56bf75
ZZAMBAs <rksidksrksi@naver.com>
2024-01-02 00:52:14 +0900

    Refactor: BASE62의 62를 상수로 관리한다.

--- src/main/java/org/prgms/springbooturlshortener/domain/shorturl/service/transformer/Base62UrlTransformer.java
+++ src/main/java/org/prgms/springbooturlshortener/domain/shorturl/service/transformer/Base62UrlTransformer.java
@@ -8,15 +8,16 @@ import org.springframework.stereotype.Component;
 public class Base62UrlTransformer implements UrlTransformer{
     private static final String BASE62_TOKENS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     private static final short numOfUrl = 6;
+    private static final int DIVIDE_NUM = 62;
 
     public String generateUrl(int number) {
         StringBuilder sb = new StringBuilder();
 
         for (short i = 0; i < numOfUrl; i++) {
-            char token = BASE62_TOKENS.charAt(number % 62);
+            char token = BASE62_TOKENS.charAt(number % DIVIDE_NUM);
 
             sb.append(token);
-            number /= 62;
+            number /= DIVIDE_NUM;
         }
 
         return sb.toString();
