db1d23278f6f4b178ac31569c174ff0dbd690410
ZZAMBAs <rksidksrksi@naver.com>
2024-01-02 00:32:46 +0900

    Refactor: stream을 이용하여 간략화한다.

--- src/main/java/org/prgms/springbooturlshortener/domain/shorturl/common/ShortUrlUtil.java
+++ src/main/java/org/prgms/springbooturlshortener/domain/shorturl/common/ShortUrlUtil.java
@@ -1,13 +1,9 @@
 package org.prgms.springbooturlshortener.domain.shorturl.common;
 
+import java.util.Arrays;
+
 public class ShortUrlUtil {
     public static String deleteProtocol(String originalUrl) {
-        for (String protocol: ShortUrlConstant.PROTOCOLS) {
-            if (originalUrl.startsWith(protocol)) {
-                originalUrl = originalUrl.replace(protocol, "");
-            }
-        }
-
-        return originalUrl;
+        return Arrays.stream(ShortUrlConstant.PROTOCOLS).reduce(originalUrl, (url, protocol) -> url.replace(protocol, ""));
     }
 }
