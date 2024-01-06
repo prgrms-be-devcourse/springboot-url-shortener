44d67d544d7b83c1cba04978a908256767e7df90
marooo326 <ktnqkr01@gmail.com>
2024-01-06 15:55:08 +0900

    fix: 상수에 누락된 static 추가

--- src/main/java/org/daehwi/shorturl/controller/UrlController.java
+++ src/main/java/org/daehwi/shorturl/controller/UrlController.java
@@ -1,7 +1,8 @@
 package org.daehwi.shorturl.controller;
 
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.daehwi.shorturl.controller.dto.ApiResponse;
 import org.daehwi.shorturl.controller.dto.ResponseStatus;
 import org.daehwi.shorturl.controller.dto.ShortUrlRequest;
@@ -9,31 +10,36 @@ import org.daehwi.shorturl.service.UrlService;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class UrlController {
+	private final static String DEFAULT_SCHEME = "http://";
+	private final static String HOST = "localhost:8080/";
 
-    private final UrlService urlService;
-    private final String DEFAULT_SCHEME = "http://";
-    private final String HOST = "localhost:8080/";
+	private final UrlService urlService;
 
-    @PostMapping("/api/v1/short-url")
-    public ResponseEntity<ApiResponse<String>> createShortUrl(@RequestBody @Valid ShortUrlRequest requestDto) throws URISyntaxException {
-        URI shortUrl = new URI(DEFAULT_SCHEME + HOST + urlService.createShortUrl(requestDto));
-        return ResponseEntity.created(shortUrl).body(ApiResponse.of(ResponseStatus.OK, shortUrl.toString()));
-    }
+	@PostMapping("/api/v1/short-url")
+	public ResponseEntity<ApiResponse<String>> createShortUrl(@RequestBody @Valid ShortUrlRequest requestDto) throws
+		URISyntaxException {
+		URI shortUrl = new URI(DEFAULT_SCHEME + HOST + urlService.createShortUrl(requestDto));
+		return ResponseEntity.created(shortUrl).body(ApiResponse.of(ResponseStatus.OK, shortUrl.toString()));
+	}
 
-    @GetMapping("/{shortUrl}")
-    public ResponseEntity<ApiResponse<Void>> getOriginUrl(@PathVariable String shortUrl) {
-        String originUrl = urlService.getOriginUrl(shortUrl);
-        return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT)
-                .cacheControl(CacheControl.noCache())
-                .header("Location", "http://" + originUrl)
-                .body(ApiResponse.of(ResponseStatus.TEMPORARY_REDIRECT));
-    }
+	@GetMapping("/{shortUrl}")
+	public ResponseEntity<ApiResponse<Void>> getOriginUrl(@PathVariable String shortUrl) {
+		String originUrl = urlService.getOriginUrl(shortUrl);
+		return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT)
+			.cacheControl(CacheControl.noCache())
+			.header("Location", DEFAULT_SCHEME + originUrl)
+			.body(ApiResponse.of(ResponseStatus.TEMPORARY_REDIRECT));
+	}
 }
