55ca64c2690aaf4e0f7022e0b7ad6017f84e43e0
marooo326 <ktnqkr01@gmail.com>
2024-01-06 16:27:25 +0900

    fix: EncodeType의 code 제거, offset 활용방식 변경

--- src/main/java/org/daehwi/shorturl/controller/dto/EncodeType.java
+++ src/main/java/org/daehwi/shorturl/controller/dto/EncodeType.java
@@ -1,15 +1,5 @@
 package org.daehwi.shorturl.controller.dto;
 
 public enum EncodeType {
-    BASE62(0), BASE32(1);
-
-    private final int code;
-
-    EncodeType(int code) {
-        this.code = code;
-    }
-
-    public int getCode() {
-        return code;
-    }
+	BASE62, BASE32
 }
--- src/main/java/org/daehwi/shorturl/service/UrlService.java
+++ src/main/java/org/daehwi/shorturl/service/UrlService.java
@@ -1,7 +1,9 @@
 package org.daehwi.shorturl.service;
 
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.Optional;
+
 import org.daehwi.shorturl.controller.dto.EncodeType;
 import org.daehwi.shorturl.controller.dto.ResponseStatus;
 import org.daehwi.shorturl.controller.dto.ShortUrlRequest;
@@ -14,50 +16,51 @@ import org.daehwi.shorturl.util.encoder.Base62Encoder;
 import org.daehwi.shorturl.util.encoder.Encoder;
 import org.springframework.stereotype.Service;
 
-import java.math.BigInteger;
-import java.util.Map;
-import java.util.Optional;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UrlService {
 
-    private static final int DEFAULT_HASH_SIZE = 8;
+	private static final int DEFAULT_HASH_SIZE = 8;
 
-    private final UrlRepository urlRepository;
+	private final UrlRepository urlRepository;
 
-    private final Map<EncodeType, Encoder> encoderMap = Map.of(
-            EncodeType.BASE32, new Base32Encoder(),
-            EncodeType.BASE62, new Base62Encoder()
-    );
+	private final Map<EncodeType, Encoder> encoderMap = Map.of(
+		EncodeType.BASE32, new Base32Encoder(),
+		EncodeType.BASE62, new Base62Encoder()
+	);
 
-    public String createShortUrl(ShortUrlRequest requestDto) {
-        final EncodeType encodeType = requestDto.getEncodeType();
-        final Encoder encoder = encoderMap.get(encodeType);
+	public String createShortUrl(ShortUrlRequest requestDto) {
+		final EncodeType encodeType = requestDto.getEncodeType();
+		final Encoder encoder = encoderMap.get(encodeType);
 
-        final String originUrl = UrlValidator.validateUrl(requestDto.getUrl());
-        final BigInteger id = getUniqueId(originUrl, encodeType.getCode());
-        String shortUrl = encoder.encode(id);
-        urlRepository.save(new ShortUrl(id.longValue(), originUrl, shortUrl));
-        return shortUrl;
-    }
+		final String originUrl = UrlValidator.validateUrl(requestDto.getUrl());
+		final BigInteger id = getUniqueId(originUrl, encodeType.name());
+		String shortUrl = encoder.encode(id);
+		urlRepository.save(new ShortUrl(id.longValue(), originUrl, shortUrl));
+		return shortUrl;
+	}
 
-    public String getOriginUrl(String shortUrl) {
-        final ShortUrl urlEntity = urlRepository.findByShortUrl(shortUrl)
-                .orElseThrow(() -> new CustomException(ResponseStatus.SHORT_URL_NOT_FOUND));
-        urlEntity.increaseRequestCount();
-        return urlEntity.getOriginUrl();
-    }
+	public String getOriginUrl(String shortUrl) {
+		final ShortUrl urlEntity = urlRepository.findByShortUrl(shortUrl)
+			.orElseThrow(() -> new CustomException(ResponseStatus.SHORT_URL_NOT_FOUND));
+		urlEntity.increaseRequestCount();
+		return urlEntity.getOriginUrl();
+	}
 
-    private BigInteger getUniqueId(String originUrl, int offset) {
-        while (true) {
-            final BigInteger id = Encoder.sha256Hash(originUrl, DEFAULT_HASH_SIZE + offset);
-            final Optional<ShortUrl> shortUrl = urlRepository.findById(id.longValue());
-            if (shortUrl.isEmpty() || shortUrl.get().getOriginUrl().equals(originUrl)) {
-                return id;
-            }
-            ++offset;
-        }
-    }
+	private BigInteger getUniqueId(String originUrl, String encodeType) {
+		int offset = 0;
+		while (true) {
+			final BigInteger id = Encoder.sha256Hash(originUrl + encodeType, DEFAULT_HASH_SIZE + offset);
+			System.out.println(id);
+			final Optional<ShortUrl> shortUrl = urlRepository.findById(id.longValue());
+			if (shortUrl.isEmpty() || shortUrl.get().getOriginUrl().equals(originUrl)) {
+				return id;
+			}
+			++offset;
+		}
+	}
 }
