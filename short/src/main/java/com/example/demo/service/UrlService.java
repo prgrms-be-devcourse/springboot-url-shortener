912050518a115ca6712902e3d517d4abb414761f
hi-june <wnsgur735@gmail.com>
2024-01-29 20:00:36 +0900

    refactor: prefix를 value 어노테이션을 통해 가져오도록 수정

--- short/src/main/java/com/example/demo/service/UrlService.java
+++ short/src/main/java/com/example/demo/service/UrlService.java
@@ -1,37 +1,44 @@
 package com.example.demo.service;
 
-import com.example.demo.dto.UrlRequestDTO;
-import com.example.demo.dto.UrlResponseDTO;
+import com.example.demo.dto.UrlRequest;
+import com.example.demo.dto.UrlResponse;
 import com.example.demo.entity.Url;
 import com.example.demo.repository.UrlRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UrlService {
-    private final UrlRepository urlRepository;
     private static final char[] words = {
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
     };
 
+    private final UrlRepository urlRepository;
+
+    @Value("${domain.prefix}")
+    private String prefix;
+
     @Transactional
-    public UrlResponseDTO saveUrl(UrlRequestDTO urlRequestDTO) {
-        String orgUrl = urlRequestDTO.orgUrl();
+    public UrlResponse saveUrl(UrlRequest urlRequest) {
+        String orgUrl = urlRequest.orgUrl();
         Url url = new Url(orgUrl);
 
         Url savedUrl = urlRepository.save(url);
         Long urlIndex = savedUrl.getId();
-        String shortUrl = base62(urlIndex);
+        String encoded = base62(urlIndex);
 
-        savedUrl.setEncoded(shortUrl);
+        savedUrl.setEncoded(encoded);
 
-        return UrlResponseDTO.of("https://dev.choi/" + savedUrl.getEncoded(), savedUrl.getUrl());
+        String shortUrl = prefix + savedUrl.getEncoded();
+
+        return UrlResponse.of(shortUrl, savedUrl.getOriginalUrl());
     }
 
     @Transactional(readOnly = true)
@@ -39,17 +46,17 @@ public class UrlService {
         Url url = urlRepository.findByEncoded(encoded)
                 .orElseThrow(() -> new RuntimeException("no such Url"));
 
-        return url.getUrl();
+        return url.getOriginalUrl();
     }
 
     private String base62(Long urlIndex) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
         while (urlIndex % 62 > 0 || result.equals("")) { // index가 62인 경우에도 적용되기 위해 do-while 형식이 되도록 구현
-            result = result + words[urlIndex.intValue() % 62];
-            urlIndex = (Long) (urlIndex / 62);
+            result.append(words[urlIndex.intValue() % 62]);
+            urlIndex = urlIndex / 62;
         }
 
-        return result;
+        return result.toString();
     }
 }
