e0eff9d79b7c5002efc7d4995d2a4741fb5e155d
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2024-02-01 16:12:35 +0900

    refactor : id채번 전략 변경

--- urlshortener/src/main/java/org/prgrms/urlshortener/domain/Url.java
+++ urlshortener/src/main/java/org/prgrms/urlshortener/domain/Url.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class Url {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(name = "origin_url", updatable = false, nullable = false)
